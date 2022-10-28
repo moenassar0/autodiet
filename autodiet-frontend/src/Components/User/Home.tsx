43da9a25fac42246995fba0b8dae65e6c92a56e8
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 09:20:28 +0300

    frontend: sidebar navigation added

--- autodiet-frontend/src/Components/User/Home.tsx
+++ autodiet-frontend/src/Components/User/Home.tsx
@@ -4,9 +4,17 @@ export const Home = () => {
             <div className="sidenavbar">
                 <div className="sidenavbar-top"><img src="../logo2.png"></img></div>
                 <div className="sidenavbar-items scrollbar">
+                    <div className="sidenavbar-item"></div>
+                    <div className="sidenavbar-item">Username</div>
+                    <div className='sidenavbar-item'><hr></hr></div>
                     <div className="sidenavbar-item">Meals</div>
                     <div className="sidenavbar-item">Profile</div>
-   
+                    <div className="sidenavbar-item">Preferences</div>
+                    <div className='sidenavbar-item'><hr></hr></div>
+                    <div className="sidenavbar-item">Browse meals</div>
+                    <div className="sidenavbar-item">Browse food items</div>
+                    <div className="sidenavbar-item">Browse diets</div>
+                    <div className='sidenavbar-item'><hr></hr></div>
                 </div> 
             </div>
             <div className="topnavbar"></div>
