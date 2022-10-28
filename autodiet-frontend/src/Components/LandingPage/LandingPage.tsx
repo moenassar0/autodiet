bf9b8d136bab752a7a6d6c78887321cb000d9e25
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 07:19:44 +0300

    frontend: added sidenavbar styling

--- autodiet-frontend/src/Components/LandingPage/LandingPage.tsx
+++ autodiet-frontend/src/Components/LandingPage/LandingPage.tsx
@@ -14,10 +14,10 @@ export const LandingPage = () => {
         <Navbar />
         <div className='navbar-line'><hr></hr></div>
         <div className='hero-content'>
-        <span className='hero-title'>AUTOMATE YOUR DIET</span>
-        <span className='hero-subtitle'>You don't have to waste your time.<br></br>We'll do the work for you.</span>
-        <span className='hero-description'>With a click of a button, generate the next<br />week’s entire meal plan and grocesseies list:</span>
-        <img className='hero-image' src="../../hero.png"></img>
+          <span className='hero-title'>AUTOMATE YOUR DIET</span>
+          <span className='hero-subtitle'>You don't have to waste your time.<br></br>We'll do the work for you.</span>
+          <span className='hero-description'>With a click of a button, generate the next<br />week’s entire meal plan and grocesseies list:</span>
+          <img className='hero-image' src="../../hero.png"></img>
         </div>
       </>
     )
