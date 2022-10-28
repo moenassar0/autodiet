6a1c42f5c939c2248f979b9acf60b9583ffebbc1
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 08:50:53 +0300

    frontend: added sidenavbar logo

--- autodiet-frontend/src/Components/User/Home.tsx
+++ autodiet-frontend/src/Components/User/Home.tsx
@@ -1,9 +1,11 @@
 export const Home = () => {
     return(
-        <>
+        <div className="flex">
             <div className="sidenavbar">
+                <div className="sidenav-bar-top"><img src="../logo2.png"></img></div>
+
             </div>
-            <div className=""></div>
-        </>
+            <div className="topnavbar"></div>
+        </div>
     )
 }
