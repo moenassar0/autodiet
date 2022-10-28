fd922fad5bcaab6344bfb7119d558e26e34a7856
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 06:54:46 +0300

    frontend: navbar component implemented

---
+++ autodiet-frontend/src/Components/LandingPage/Navbar.tsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+    return(
+        <div className='navbar'>
+        <div className='navbar-icon'></div>
+        <ul>
+          <li>Why use AutoDiet?</li>
+          <li>Features</li>
+          <li>Features</li>
+        </ul>
+        <div className='navbar-register'>
+          <button>Sign Up</button>
+          <span>Already have an account? Login.</span>
+        </div>
+      </div>
+    )
+}
