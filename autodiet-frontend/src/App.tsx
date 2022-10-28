d2d4426bf64f045371803429dac5b382509307fe
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 04:01:35 +0300

    frontend: navbar added

--- autodiet-frontend/src/App.tsx
+++ autodiet-frontend/src/App.tsx
@@ -7,7 +7,18 @@ function App() {
   return (
     <>
       <div className='translucent-overlay'></div>
-
+      <div className='navbar'>
+        <div className='navbar-icon'></div>
+        <ul>
+          <li>Why use AutoDiet?</li>
+          <li>Features</li>
+          <li>Features</li>
+        </ul>
+        <div className='navbar-register'>
+          <button>Sign Up</button>
+          <span>Already have an account? Login.</span>
+        </div>
+      </div>
     </>
   );
 }
