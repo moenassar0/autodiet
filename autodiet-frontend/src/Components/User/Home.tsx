994c7d4498b73286eaecb45855d912eefda58f25
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 09:12:11 +0300

    frontend: added sidebar items

--- autodiet-frontend/src/Components/User/Home.tsx
+++ autodiet-frontend/src/Components/User/Home.tsx
@@ -2,8 +2,12 @@ export const Home = () => {
     return(
         <div className="flex">
             <div className="sidenavbar">
-                <div className="sidenav-bar-top"><img src="../logo2.png"></img></div>
-
+                <div className="sidenavbar-top"><img src="../logo2.png"></img></div>
+                <div className="sidenavbar-items scrollbar">
+                    <div className="sidenavbar-item">Meals</div>
+                    <div className="sidenavbar-item">Profile</div>
+   
+                </div> 
             </div>
             <div className="topnavbar"></div>
         </div>
