172901187efd30f9c2ba7f8b64759400ce0ed055
Mohamad Nassar <mnassar57@gmail.com>
2022-10-28 06:53:47 +0300

    frontend: routes added

--- autodiet-frontend/src/App.tsx
+++ autodiet-frontend/src/App.tsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import './App.css';
-import { LandingPageNavBar } from './Components/LandingPage/LandingPageNavBar';
 import { Navbar } from './Components/LandingPage/Navbar';
+import { Routes, Route } from "react-router-dom";
+import { Home } from './Components/User/Home';
+import { LandingPage } from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <>
-      <div className='translucent-overlay'></div>
-      <Navbar />
-      <div className='navbar-line'><hr></hr></div>
-      <div className='hero-content'>
-        <span className='hero-title'>AUTOMATE YOUR DIET</span>
-        <span className='hero-subtitle'>You don't have to waste your time.<br></br>We'll do the work for you.</span>
-        <span className='hero-description'>With a click of a button, generate the next<br />week’s entire meal plan and grocesseies list:</span>
-        <img className='hero-image' src="../../hero.png"></img>
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
     </>
   );
 }
