97bc23077239578815064a46547262468c1af2e9
Mohamad Nassar <mnassar57@gmail.com>
2022-10-29 18:33:28 +0300

    frontend: register form button added

--- autodiet-frontend/src/Components/Register.tsx
+++ autodiet-frontend/src/Components/Register.tsx
@@ -11,7 +11,7 @@ export const Register = () => {
                 <div className="flex items-center justify-center w-full h-12 bg-black mb-8 rounded-t text-ad-golden font-bold text-lg">
                     Create a new account
                 </div>
-                <div className="flex flex-col w-full justify-center gap-10">
+                <div className="flex flex-col w-full justify-center gap-10 px-2">
                     <div className="flex items-center place-content-between w-full h-10">
                         <span className="w-2/5 text-ad-golden text-lg">Email</span>
                         <input className="w-3/5 py-2 px-4 bg-black text-white"type="text"></input>
@@ -19,6 +19,9 @@ export const Register = () => {
                     <div className="flex items-center place-content-between w-full h-10">
                         <span className="w-2/5 text-ad-golden text-lg">Repeat password</span>
                         <input className="w-3/5 py-2 px-4 bg-black text-white"type="text"></input>
+                    </div>
+                    <div className="flex items-center self-end w-3/5 h-10">
+                        <button className="w-full h-full bg-ad-golden text-black rounded">Register</button>
                     </div>
                 </div>
             </div>
