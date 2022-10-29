3fa46b921630ef323bcb5eefd9b1a2762cbcb0ed
Mohamad Nassar <mnassar57@gmail.com>
2022-10-29 17:41:50 +0300

    frontend: login form title added

--- autodiet-frontend/src/Components/Login.tsx
+++ autodiet-frontend/src/Components/Login.tsx
@@ -7,6 +7,11 @@ export const Login = () => {
             <Navbar />
             <div className='navbar-line'><hr></hr></div>
             <div className='hero-content flex items-center px-6'>
+                <div className="flex flex-col w-full max-w-lg h-96 items-center bg-ad-lightgrey rounded-t">
+                    <div className="flex items-center justify-center w-full h-12 bg-black mb-8 rounded-t text-ad-golden font-bold text-lg">
+                        LOGIN
+                    </div>
+                </div>
                 <img className='hero-image' src="../../hero.png"></img>
             </div>
         </>
