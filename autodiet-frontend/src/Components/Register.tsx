fe175b10d3208d15af96d268c997f090d116de82
Mohamad Nassar <mnassar57@gmail.com>
2022-10-29 18:20:23 +0300

    frontend: register component updated

--- autodiet-frontend/src/Components/Register.tsx
+++ autodiet-frontend/src/Components/Register.tsx
@@ -1,5 +1,21 @@
+import { Navbar } from "./LandingPage/Navbar"
+
 export const Register = () => {
     return(
-        <>Regis</>
+        <>
+        <div className='translucent-overlay'></div>
+        <Navbar />
+        <div className='navbar-line'><hr></hr></div>
+        <div className='hero-content flex items-center px-6'>
+            <div className="flex flex-col w-full max-w-lg h-96 items-center bg-ad-lightgrey rounded-t">
+                <div className="flex items-center justify-center w-full h-12 bg-black mb-8 rounded-t text-ad-golden font-bold text-lg">
+                    Create a new account
+                </div>
+                <div className="flex flex-col w-full items-center gap-10">
+                </div>
+            </div>
+            <img className='hero-image' src="../../hero.png"></img>
+        </div>
+    </>
     )
 }
