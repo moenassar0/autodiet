c4b2a54a9c1403b4a20b52da59b452bba6f40cf6
Mohamad Nassar <mnassar57@gmail.com>
2022-10-29 16:12:38 +0300

    frontend: login route added

--- autodiet-frontend/src/App.tsx
+++ autodiet-frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Navbar } from './Components/LandingPage/Navbar';
 import { Routes, Route } from "react-router-dom";
 import { Home } from './Components/User/Home';
 import { LandingPage } from './Components/LandingPage/LandingPage';
+import { Login } from './Components/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </>
   );
