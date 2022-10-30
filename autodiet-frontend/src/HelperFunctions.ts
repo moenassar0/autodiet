32887254edab4fd8f4b9838a0f23b8ff331087e9
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 12:27:32 +0200

    frontend: check password function added

--- autodiet-frontend/src/HelperFunctions.ts
+++ autodiet-frontend/src/HelperFunctions.ts
@@ -1,14 +1,19 @@
 //Declare helper function's variables
 const EMAIL_REGEX = /.+@.+\..+/;
+const PASSWORD_REGEX = /[A-Z]/;
 
 
-
-export const validEmail = (email:string):boolean => {
+export function checkEmail(email:string):boolean {
     if(EMAIL_REGEX.test(email)) return true
     return false;
 }
 
 export const checkStringLength = (string:string, length:number):boolean => {
     if(string.length >= length) return true;
+    return false;
+}
+
+export const checkPassword = (password:string):boolean => {
+    if(PASSWORD_REGEX.test(password) && checkStringLength(password, 8)) return true;
     return false;
 }
