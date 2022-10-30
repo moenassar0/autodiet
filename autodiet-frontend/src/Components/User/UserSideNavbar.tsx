269d0d3afbb69528120ba98aa344ffd5acd7e106
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 18:24:13 +0200

    frontend: navbar link updated

--- autodiet-frontend/src/Components/User/UserSideNavbar.tsx
+++ autodiet-frontend/src/Components/User/UserSideNavbar.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom"
 export const UserSideNavbar = () =>{
     return(
         <div className="sidenavbar">
-            <div className="sidenavbar-top"><img src="../logo2.png"></img></div>
+            <div className="flex items-center justify-center h-1/5 w-full"><img className="h-full w-full" src="../logo2.png"></img></div>
             <div className="sidenavbar-items scrollbar">
                 <div className="sidenavbar-item"></div>
                 <div className="sidenavbar-item">Username</div>
                 <div className='sidenavbar-item'><hr></hr></div>
-                <div className="sidenavbar-item">Meals</div>
+                <Link to="/user/home"><div className="sidenavbar-item">Meals</div></Link>
                 <Link to="/user/profile"><div className="cursor-pointer sidenavbar-item">Profile</div></Link>
                 <div className="sidenavbar-item">Preferences</div>
                 <div className='sidenavbar-item'><hr></hr></div>
                 <div className="sidenavbar-item">Browse meals</div>
-                <div className="sidenavbar-item">Browse food items</div>
+                <div className="sidenavbar-item">Browse foods</div>
                 <div className="sidenavbar-item">Browse diets</div>
                 <div className='sidenavbar-item'><hr></hr></div>
             </div>
