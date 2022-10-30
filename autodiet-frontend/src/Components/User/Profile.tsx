28588c080cef96c3574af7a2ee9666ca2f5feb47
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 18:34:49 +0200

    frontend: profile base updated

--- autodiet-frontend/src/Components/User/Profile.tsx
+++ autodiet-frontend/src/Components/User/Profile.tsx
@@ -1,7 +1,15 @@
+import { UserSideNavbar } from "./UserSideNavbar"
+
 export const Profile = () => {
     return(
-        <div className="flex bg-black h-screen w-full">
-        
+        <div className="flex h-min-screen w-full">
+            <UserSideNavbar />
+            <div className="flex flex-col h-screen w-4/5">
+                <div className="topnavbar"></div>
+                <div className="flex h-4/5 w-full bg-ad-lightgrey">
+                    
+                </div>
+            </div>
         </div>
     )
 }
