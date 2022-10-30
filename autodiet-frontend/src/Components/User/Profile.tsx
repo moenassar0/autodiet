ce982dbba047abd05db54d7545f031b3515bc220
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 20:37:30 +0200

    frontend: toggle buttons added

--- autodiet-frontend/src/Components/User/Profile.tsx
+++ autodiet-frontend/src/Components/User/Profile.tsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
 import { UserSideNavbar } from "./UserSideNavbar"
 
 export const Profile = () => {
+
+    const [activeGoal, setActiveGoal] = useState('');
+    const [sex, setSex] = useState('');
+
+    useEffect(() => {
+        console.log(activeGoal);
+    }, [activeGoal])
+
     return(
         <div className="flex h-min-screen w-full">
             <UserSideNavbar />
@@ -11,17 +20,16 @@ export const Profile = () => {
                         <div className="flex w-full h-1/6">
                             <div className="flex items-center  w-1/5 h-full">Goal</div>
                             <div className="flex w-3/5 h-full items-center justify-end">
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3 rounded-l-lg">Lose weight</button>
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3">Maintain</button>
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3 rounded-r-lg">Gain muscle</button>
+                                <button onClick={() => {setActiveGoal("LoseWeight")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (activeGoal==="LoseWeight" ? "bg-ad-golden" : "bg-white")}>Lose weight</button>
+                                <button onClick={() => {setActiveGoal("Maintain")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (activeGoal==="Maintain" ? "bg-ad-golden" : "bg-white")}>Maintain</button>
+                                <button onClick={() => {setActiveGoal("GainMuscle")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-r-lg " + (activeGoal==="GainMuscle" ? "bg-ad-golden" : "bg-white")}>Gain muscle</button>
                             </div>
                         </div>
                         <div className="flex w-full h-1/6">
                             <div className="flex items-center  w-1/5 h-full">Goal</div>
                             <div className="flex w-3/5 h-full items-center justify-end">
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3 rounded-l-lg">Lose weight</button>
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3">Maintain</button>
-                                <button className="flex items-center justify-center bg-ad-golden h-1/2 justify-self-end w-1/3 rounded-r-lg">Gain muscle</button>
+                                <button onClick={() => {setActiveGoal("LoseWeight")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (activeGoal==="LoseWeight" ? "bg-ad-golden" : "bg-white")}>Lose weight</button>
+                                <button onClick={() => {setActiveGoal("Maintain")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (activeGoal==="Maintain" ? "bg-ad-golden" : "bg-white")}>Maintain</button>
                             </div>
                         </div>
                     </div>
