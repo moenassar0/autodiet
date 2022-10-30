611370752366a1b593a1a2d779a0f277bad84859
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 23:55:02 +0200

    frontend: tailwind and dependencies added

--- autodiet-frontend/tailwind.config.js
+++ autodiet-frontend/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "ad-golden": "#FDAD00",
+        "ad-darkgrey": "#141414",
+        "ad-lightgrey": "#363636"
+      }
+    },
   },
   plugins: [],
 }
