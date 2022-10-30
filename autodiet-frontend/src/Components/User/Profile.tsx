4514ec27ead92f572eb7233f2b5dedb90753dba0
Mohamad Nassar <mnassar57@gmail.com>
2022-10-30 20:56:55 +0200

    frontend: profile base implemented

--- autodiet-frontend/src/Components/User/Profile.tsx
+++ autodiet-frontend/src/Components/User/Profile.tsx
@@ -5,6 +5,9 @@ export const Profile = () => {
 
     const [activeGoal, setActiveGoal] = useState('');
     const [sex, setSex] = useState('');
+    const [weight, setWeight] = useState('');
+    const [age, setAge] = useState('');
+    const [bodyFatPercentage, setBodyFatPercentage] = useState('');
 
     useEffect(() => {
         console.log(activeGoal);
@@ -18,7 +21,7 @@ export const Profile = () => {
                 <div className="flex h-4/5 w-full bg-ad-lightgrey">
                     <div className="flex flex-col h-4/5 w-full py-2 px-2">
                         <div className="flex w-full h-1/6">
-                            <div className="flex items-center  w-1/5 h-full">Goal</div>
+                            <div className="flex items-center  w-1/5 h-full text-white">Goal</div>
                             <div className="flex w-3/5 h-full items-center justify-end">
                                 <button onClick={() => {setActiveGoal("LoseWeight")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (activeGoal==="LoseWeight" ? "bg-ad-golden" : "bg-white")}>Lose weight</button>
                                 <button onClick={() => {setActiveGoal("Maintain")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (activeGoal==="Maintain" ? "bg-ad-golden" : "bg-white")}>Maintain</button>
@@ -26,10 +29,16 @@ export const Profile = () => {
                             </div>
                         </div>
                         <div className="flex w-full h-1/6">
-                            <div className="flex items-center  w-1/5 h-full">Goal</div>
+                            <div className="flex items-center  w-1/5 h-full text-white">Sex</div>
                             <div className="flex w-3/5 h-full items-center justify-end">
-                                <button onClick={() => {setActiveGoal("LoseWeight")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (activeGoal==="LoseWeight" ? "bg-ad-golden" : "bg-white")}>Lose weight</button>
-                                <button onClick={() => {setActiveGoal("Maintain")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (activeGoal==="Maintain" ? "bg-ad-golden" : "bg-white")}>Maintain</button>
+                                <button onClick={() => {setSex("Male")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-l-lg " + (sex ==="Male" ? "bg-ad-golden" : "bg-white")}>Male</button>
+                                <button onClick={() => {setSex("Female")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-r-lg " + (sex === "Female" ? "bg-ad-golden" : "bg-white")}>Female</button>
+                            </div>
+                        </div>
+                        <div className="flex items-center w-full h-1/6">
+                            <div className="flex items-center w-1/5 h-full text-white">Height</div>
+                            <div className="flex w-3/5 h-1/2 justify-start">
+                                <input type="text" className="w-1/2 h-full rounded"></input>
                             </div>
                         </div>
                     </div>
