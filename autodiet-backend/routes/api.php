9cb6d5c1a92731f7665bf1095bedac9221966500
Mohamad Nassar <mnassar57@gmail.com>
2022-10-31 17:05:36 +0200

    backend: updated imports

--- autodiet-backend/routes/api.php
+++ autodiet-backend/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\MealController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
