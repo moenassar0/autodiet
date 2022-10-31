b416a97554009bc574fbc7d71bba445cac7bc6f8
Mohamad Nassar <mnassar57@gmail.com>
2022-10-31 16:50:36 +0200

    frontend: base loop added

--- autodiet-frontend/src/PlanGenerator.ts
+++ autodiet-frontend/src/PlanGenerator.ts
@@ -1,4 +1,4 @@
-import { getRandomInt } from "./HelperFunctions";
+import { getRandomInt, multiplier } from "./HelperFunctions";
 
 interface MealProps {
     id: number,
@@ -17,6 +17,26 @@ function PlanGenerator (meals:Array<MealProps>):any{
     let proteinPercentage = getRandomInt(18,40);
     let acceptableError = 50;
     let protein = 0;
+
+    console.log("protein got: ", protein, "protein wanted: ", (proteinPercentage/100 * totalcalories)/4)
+///////////////////////////////////
+
+async function find(loops:number){
+        if(loops >= 20){
+            console.log("timeout");
+            acceptableError += 25
+        }
+        if(loops >= 100){
+            console.log("Couldn't find!");
+            return [];
+        }
+        let calories = totalcalories;
+        mealplan = [];
+        protein = 0;
+        while(calories > 50){
+    
+        }
+    }
 }
 
 export default PlanGenerator;
