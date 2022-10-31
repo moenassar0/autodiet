e13ed83325f3cb2d8f3a6e3a6c78c446dab4fa91
Mohamad Nassar <mnassar57@gmail.com>
2022-10-31 16:41:59 +0200

    frontend: plan generator base variables added

--- autodiet-frontend/src/PlanGenerator.ts
+++ autodiet-frontend/src/PlanGenerator.ts
@@ -1,3 +1,5 @@
+import { getRandomInt } from "./HelperFunctions";
+
 interface MealProps {
     id: number,
     title: string,
@@ -6,7 +8,15 @@ interface MealProps {
     carbohydrate: number,
     fat: number
 }
-function PlanGenerator (meals:Array<MealProps>):any {
+function PlanGenerator (meals:Array<MealProps>):any{
+    let tempMeals = meals;
+    let totalcalories = 2000;
+    let mealplan:Array<MealProps> = [];
+    let mealsAmount = getRandomInt(3,7);
+    let calorieForEachmeal = totalcalories/mealsAmount;
+    let proteinPercentage = getRandomInt(18,40);
+    let acceptableError = 50;
+    let protein = 0;
 }
 
 export default PlanGenerator;
