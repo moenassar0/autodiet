2bd4fb978057cfe1b4d546e8ba54359e89bbdd15
moenassar0 <mnassar57@gmail.com>
2022-11-01 10:37:59 +0200

    frontend: meal recipe popup added

--- autodiet-frontend/src/Components/User/MealRecipePopup.tsx
+++ autodiet-frontend/src/Components/User/MealRecipePopup.tsx
@@ -1,9 +1,17 @@
 export const MealRecipePopup = () => {
     return(
-        <div className="absolute flex justify-center items-center bg-black top-0 left-0 h-screen w-full opacity-50 z-10">
-            <div className="relative bg-black flex flex-col h-96 w-1/2">
-                <div className="flex w-full h-1/6 bg-ad-golden"></div>
+        <>
+        <div className="absolute bg-black top-0 left-0 h-screen w-full opacity-50 z-10"></div>
+        <div className="absolute top-0 left-0 flex justify-center items-center h-screen w-full z-20">
+            <div className="w-full h-4/5 px-2 sm:w-1/2 relative bg-ad-lightgrey flex flex-col h-96 rounded">
+                <div className="h-11 px-2 sm:flex items-center justify-between w-full h-1/6">
+                    <span className="flex justify-start items-center h-full w-1/6 text-ad-golden">Meal X</span>
+                    <span className="flex justify-center items-center h-5/6 w-10 text-ad-golden cursor-pointer rounded-full hover:bg-ad-hoveredblack">X</span>
+                </div>
+                <div className='px-2'><hr className="border border-ad-golden"></hr></div>
             </div>
         </div>
+        </>
+
     )
 }
