53c3d651bc693bc706a94581514bc7d0d0ddd5e2
moenassar0 <mnassar57@gmail.com>
2022-11-01 08:47:51 +0200

    backend: routes updated

--- autodiet-backend/routes/api.php
+++ autodiet-backend/routes/api.php
@@ -6,25 +6,12 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MealController;
 use App\Http\Middleware\IsUser;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Middleware\IsAdmin;
 
 Route::post("/users", [UserController::class, "register"]);
 Route::post("/login", [AuthController::class, "login"]);
+Route::get("/me", [AuthController::class, "me"]);
 
-
-Route::middleware([IsUser::class])->group(function () {
+Route::middleware([IsAdmin::class])->group(function () {
     Route::get("/meals", [MealController::class, "getMeals"]);
 });
