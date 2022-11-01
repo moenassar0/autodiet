0c8a048c5399386587fb01e41ca867644056fdaf
moenassar0 <mnassar57@gmail.com>
2022-11-01 09:50:17 +0200

    frontend: meal recipe popup base added

---
+++ autodiet-frontend/src/Components/User/MealRecipePopup.tsx
@@ -0,0 +1,9 @@
+export const MealRecipePopup = () => {
+    return(
+        <div className="absolute flex justify-center items-center bg-black top-0 left-0 h-screen w-full opacity-50 z-10">
+            <div className="relative bg-black flex flex-col h-96 w-1/2">
+                <div className="flex w-full h-1/6 bg-ad-golden"></div>
+            </div>
+        </div>
+    )
+}
