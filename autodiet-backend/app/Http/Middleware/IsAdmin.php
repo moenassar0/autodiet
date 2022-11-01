869135982fee4638e1aa28c98e822a45fd22e9a2
moenassar0 <mnassar57@gmail.com>
2022-11-01 08:33:19 +0200

    backend: is-admin middleware added

--- autodiet-backend/app/Http/Middleware/IsAdmin.php
+++ autodiet-backend/app/Http/Middleware/IsAdmin.php
@@ -16,6 +16,12 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
+        if (!auth()->user()) 
+        return response()->json('Unauthorized');
+
+        if(!auth()->user()->user_role === "User") 
+            return response()->json('Unauthorized');
+
         return $next($request);
     }
 }
