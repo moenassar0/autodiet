541290a7cbfb86d9cbc50f5a69a57e3dc8a01ee4
moenassar0 <mnassar57@gmail.com>
2022-11-01 09:49:59 +0200

    frontend: popup functionality added

--- autodiet-frontend/src/Components/User/Meal.tsx
+++ autodiet-frontend/src/Components/User/Meal.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { MealRecipePopup } from "./MealRecipePopup";
+
 interface MealProps {
     id: number,
     title: string,
@@ -12,20 +15,27 @@ type Props = {
 };
 
 const Meal: React.FC<Props> = ({meal}) => {
+
+    const [popup, setPopup] = useState(false);
+
     return(
-        <div className="meal">
-            <div className="meal-title">
-                <span>Meal {}</span>
-                <span className="text-small">{meal.calories} Calories</span>
+        
+        <section onClick={() => {setPopup(!popup)}}>
+            {popup ? <MealRecipePopup /> : ""}
+            <div className="meal cursor-pointer hover:bg-ad-hoveredblack">
+                <div className="meal-title">
+                    <span>Meal {}</span>
+                    <span className="text-small">{meal.calories} Calories</span>
+                </div>
+                <div className="meal-content">
+                    <img src="../logo512.png"></img>
+                    <span>{meal.title}</span>
+                </div>
+                <div className="meal-title">
+                    <span>Protein: {meal.protein}</span>
+                </div>
             </div>
-            <div className="meal-content">
-                <img src="../logo512.png"></img>
-                <span>{meal.title}</span>
-            </div>
-            <div className="meal-title">
-                <span>Protein: {meal.protein}</span>
-            </div>
-        </div>
+        </section>
     )
 }
 
