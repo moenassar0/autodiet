ba3c3c022173826678c176b58c4013dd1fa58ba5
moenassar0 <mnassar57@gmail.com>
2022-11-02 10:30:06 +0200

    frontend: browse meals page added

---
+++ autodiet-frontend/src/Components/User/BrowseMeals.tsx
@@ -0,0 +1,15 @@
+import { UserSideNavbar } from "./UserSideNavbar"
+
+export const BrowseMeals = () => {
+    return(
+        <div className="flex h-min-screen w-full">
+            <UserSideNavbar />
+            <div className="container">
+                <div className="topnavbar"></div>
+                <div className="px-2 flex h-16 w-full py-2 bg-black">
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
