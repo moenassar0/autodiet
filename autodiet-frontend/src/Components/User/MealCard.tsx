56037e664c01b82c80cbed34e82a57fed7de9246
moenassar0 <mnassar57@gmail.com>
2022-11-02 17:58:54 +0200

    frontend: meal card styling updated

--- autodiet-frontend/src/Components/User/MealCard.tsx
+++ autodiet-frontend/src/Components/User/MealCard.tsx
@@ -10,11 +10,11 @@ const MealCard: React.FC<Props> = ({meal}) => {
     return(
         <div className="">
             <div className="flex flex-col h-48 w-60 mr-2 mb-2 rounded-t cursor-pointer hover:opacity-70 hover:scale-105 ease-linear duration-600">
-                <div className="h-3/6 w-full rounded-t">
+                <div className="h-4/6 w-full rounded-t">
                     <img src="../oat.png" className="w-full h-full rounded-t"></img>
                 </div>
-                <div className="bg-white h-2/6 w-full"></div>
-                <div className="bg-black h-1/6 w-full rounded-b"></div>
+                <div className="bg-white h-1/6 w-full text-black text-start px-1 py-1">{meal.title}</div>
+                <div className="bg-black h-1/6 w-full rounded-b text-white text-start px-1 py-1 text-xs">{meal.calories + " Calories"}</div>
             </div>
         </div>
     )
