3f43f7c8427ee225d82fb158156a836ef4fb3a58
moenassar0 <mnassar57@gmail.com>
2022-11-02 10:49:56 +0200

    frontend: search input and function added

--- autodiet-frontend/src/Components/User/BrowseMeals.tsx
+++ autodiet-frontend/src/Components/User/BrowseMeals.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react"
 import { UserSideNavbar } from "./UserSideNavbar"
 
 export const BrowseMeals = () => {
+
+    const [searchInput, setSearchInput] = useState('');
+
+    useEffect(() => {
+        searchQuery();
+    }, [])
+
+    const searchQuery = async () => {
+        try{
+
+        }catch{
+            
+        }
+    }
+
     return(
         <div className="flex h-min-screen w-full">
             <UserSideNavbar />
             <div className="container">
                 <div className="topnavbar"></div>
-                <div className="px-2 flex h-16 w-full py-2 bg-black">
-                    
+                <div className="px-2 flex h-10 w-full py-2 bg-black">
+                    <input onChange={(e) => setSearchInput(e.target.value)} type="text" className="w-full h-full flex px-2 py-2 rounded" />
                 </div>
             </div>
         </div>
