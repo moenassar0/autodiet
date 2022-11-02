535d9556940108c82a356c9fe0610c7fb0f7116d
moenassar0 <mnassar57@gmail.com>
2022-11-02 17:21:17 +0200

    frontend: meal card css base added

--- autodiet-frontend/src/Components/User/MealCard.tsx
+++ autodiet-frontend/src/Components/User/MealCard.tsx
@@ -1,24 +1,21 @@
 import { useState } from "react";
 import { MealRecipePopup } from "./MealRecipePopup";
-
-interface MealProps {
-    id: number,
-    title: string,
-    calories: number,
-    protein: number,
-    carbohydrate: number,
-    fat: number,
-    multiplier: number,
-  }
+import { Meal } from "../../types/types";
 
 type Props = {
-    meal: MealProps
+    meal: Meal
 };
 
 const MealCard: React.FC<Props> = ({meal}) => {
     return(
         <div className="">
-            
+            <div className="flex flex-col h-48 w-60 mr-2 mb-2 rounded-t cursor-pointer hover:opacity-70 hover:scale-105 ease-linear duration-600">
+                <div className="h-3/6 w-full rounded-t">
+                    <img src="../oat.png" className="w-full h-full rounded-t"></img>
+                </div>
+                <div className="bg-white h-2/6 w-full"></div>
+                <div className="bg-black h-1/6 w-full rounded-b"></div>
+            </div>
         </div>
     )
 }
