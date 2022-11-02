9845511bd0f3b789f65b804d6b07279ee3fe7e21
moenassar0 <mnassar57@gmail.com>
2022-11-02 15:11:05 +0200

    frontend: browse meal page updated

--- autodiet-frontend/src/Components/User/BrowseMeals.tsx
+++ autodiet-frontend/src/Components/User/BrowseMeals.tsx
@@ -26,23 +26,18 @@ export const BrowseMeals = () => {
             <UserSideNavbar />
             <div className="flex flex-col h-full w-full">
             <div className="topnavbar"></div>
-                <div className="flex flex-col h-4/5 w-full bg-ad-golden px-4 py-4">
+                <div className="flex flex-col h-4/5 w-full bg-ad-lightgrey px-4 py-4">
                     <div className="px-2 flex h-10 w-full py-2">
                         <input onChange={(e) => setSearchInput(e.target.value)} type="text" className="w-full h-full flex px-2 py-2 rounded" />
                     </div>
-                    <div className="flex flex-wrap h-auto w-full bg-black overflow-auto px-4 py-4">
-                        <div className="flex flex-col h-48 w-60 mr-2 mb-2 rounded">
-                            <div className="bg-ad-lightgrey h-3/6 w-full rounded-t"></div>
-                            <div className="bg-ad-golden h-2/6 w-full"></div>
-                            <div className="bg-ad-lightgrey h-1/6 w-full rounded-b"></div>
+                    <div className="flex flex-wrap h-auto w-full overflow-auto px-4 py-4">
+                        <div className="flex flex-col h-48 w-60 mr-2 mb-2 rounded-t cursor-pointer hover:opacity-70 hover:scale-105 ease-linear duration-600">
+                            <div className="h-3/6 w-full rounded-t">
+                                <img src="../oat.png" className="w-full h-full rounded-t"></img>
+                            </div>
+                            <div className="bg-white h-2/6 w-full"></div>
+                            <div className="bg-black h-1/6 w-full rounded-b"></div>
                         </div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
-                        <div className="flex flex-col h-40 w-40 bg-ad-lightgrey mr-2 mb-2"></div>
                     </div>
                 </div>
             </div>
