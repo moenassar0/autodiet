bee193b164c088bebdacb87cdc9708cb350888b9
moenassar0 <mnassar57@gmail.com>
2022-11-03 09:31:57 +0200

    frontend: sidenavbar styling updated and changed to tailwind

--- autodiet-frontend/src/Components/User/UserSideNavbar.tsx
+++ autodiet-frontend/src/Components/User/UserSideNavbar.tsx
@@ -5,16 +5,20 @@ export const UserSideNavbar = () =>{
         <div className="sidenavbar">
             <div className="flex items-center justify-start"><div className="navbar-icon"></div></div>
             <div className="sidenavbar-items scrollbar">
-                
                 <div className="flex w-full h-16 text-ad-golden items-center justify-start">Username</div>
-
+                <div className='flex w-full h-8 text-ad-golden items-center justify-start'>
+                    <hr className="border w-full border-ad-golden"></hr>
+                </div>
                 <Link to="/user/home"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Meals</div></Link>
                 <Link to="/user/profile"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Profile</div></Link>
-
+                <div className='flex w-full h-8 text-ad-golden items-center justify-start'>
+                    <hr className="border w-full border-ad-golden"></hr>
+                </div>
                 <Link to="/user/meals"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Browse meals</div></Link>
                 <Link to="/user/foods"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Browse foods</div></Link>
-                <div className="sidenavbar-item">Browse diets</div>
-                <div className='sidenavbar-item'><hr></hr></div>
+                <div className='flex w-full h-8 text-ad-golden items-center justify-start'>
+                    <hr className="border w-full border-ad-golden"></hr>
+                </div>
             </div>
         </div>
     )
