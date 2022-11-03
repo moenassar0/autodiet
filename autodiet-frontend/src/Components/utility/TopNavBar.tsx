3f78e7900d9958921a46d7be1a10c0a5fd71f101
moenassar0 <mnassar57@gmail.com>
2022-11-03 09:15:34 +0200

    frontend: topnavbar styling added

--- autodiet-frontend/src/Components/utility/TopNavBar.tsx
+++ autodiet-frontend/src/Components/utility/TopNavBar.tsx
@@ -1,10 +1,14 @@
 type Props = {
+    title:string,
     children:any
 };
-export const TopNavBar: React.FC<Props> = ({children}) => {
+export const TopNavBar: React.FC<Props> = ({children, title}) => {
     return(
-        <div className="flex h-1/5 w-full bg-black">
-            {children}
+        <div className="flex h-1/5 w-full bg-ad-darkgrey px-2 py-2">
+            <div className="flex w-full items-center justify-between">
+                <span className="text-2xl text-ad-golden">{title}</span>
+                {children}
+            </div>
         </div>
     )
 }
