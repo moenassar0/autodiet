e5f2639a2e51bf95bcd8617d143d0fb8c7819102
moenassar0 <mnassar57@gmail.com>
2022-11-03 09:29:00 +0200

    frontend: styling updated

--- autodiet-frontend/src/Components/User/UserSideNavbar.tsx
+++ autodiet-frontend/src/Components/User/UserSideNavbar.tsx
@@ -5,14 +5,14 @@ export const UserSideNavbar = () =>{
         <div className="sidenavbar">
             <div className="flex items-center justify-start"><div className="navbar-icon"></div></div>
             <div className="sidenavbar-items scrollbar">
-                <div className="sidenavbar-item"></div>
-                <div className="sidenavbar-item">Username</div>
-                <div className='sidenavbar-item'><hr></hr></div>
-                <Link to="/user/home"><div className="cursor-pointer sidenavbar-item hover:underline">Meals</div></Link>
-                <Link to="/user/profile"><div className="cursor-pointer sidenavbar-item hover:underline">Profile</div></Link>
-                <div className='sidenavbar-item'><hr></hr></div>
-                <Link to="/user/meals"><div className="cursor-pointer sidenavbar-item hover:underline">Browse meals</div></Link>
-                <Link to="/user/foods"><div className="cursor-pointer sidenavbar-item hover:underline">Browse foods</div></Link>
+                
+                <div className="flex w-full h-16 text-ad-golden items-center justify-start">Username</div>
+
+                <Link to="/user/home"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Meals</div></Link>
+                <Link to="/user/profile"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Profile</div></Link>
+
+                <Link to="/user/meals"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Browse meals</div></Link>
+                <Link to="/user/foods"><div className="flex w-full h-8 text-ad-golden items-center justify-start hover:underline cursor-pointer">Browse foods</div></Link>
                 <div className="sidenavbar-item">Browse diets</div>
                 <div className='sidenavbar-item'><hr></hr></div>
             </div>
