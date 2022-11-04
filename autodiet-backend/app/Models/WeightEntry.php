c0de580867c97a0b9c5dae0e7ff70a6c0c3666c9
moenassar0 <mnassar57@gmail.com>
2022-11-04 11:47:10 +0200

    backend: weight entry model updated

--- autodiet-backend/app/Models/WeightEntry.php
+++ autodiet-backend/app/Models/WeightEntry.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class WeightEntry extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'date',
+        'weight',
+    ];
 }
