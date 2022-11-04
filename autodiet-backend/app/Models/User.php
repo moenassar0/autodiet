3b3ba00ce58cfbada8ad187ceab7ec6ad6c2743f
moenassar0 <mnassar57@gmail.com>
2022-11-04 11:07:30 +0200

    backend: function for weight entries added

--- autodiet-backend/app/Models/User.php
+++ autodiet-backend/app/Models/User.php
@@ -33,6 +33,11 @@ class User extends Authenticatable implements JWTSubject
         return $this->hasOne(UserDetail::class);
     }
 
+    public function weightEntries()
+    {
+        return $this->hasMany(WeightEntry::class);
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
