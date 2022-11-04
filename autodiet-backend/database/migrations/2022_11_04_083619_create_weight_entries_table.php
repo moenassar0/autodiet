a04f4e9bf4c23fbfc0e133a07b34add1a1cd9491
moenassar0 <mnassar57@gmail.com>
2022-11-04 10:48:30 +0200

    backend: weight entries table added

---
+++ autodiet-backend/database/migrations/2022_11_04_083619_create_weight_entries_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weight_entries', function (Blueprint $table) {
+            $table
+            ->foreignId('user_id')
+            ->constrained()
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->date('date');
+            $table->float('weight', 8, 3);
+            $table->primary(array('user_id', 'date'));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weight_entries');
+    }
+};
