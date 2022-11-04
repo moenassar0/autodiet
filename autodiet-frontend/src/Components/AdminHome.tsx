35db34a9507413e40131692c962bfa42a429ed7e
moenassar0 <mnassar57@gmail.com>
2022-11-04 19:52:21 +0200

    frontend: topnav updated

--- autodiet-frontend/src/Components/AdminHome.tsx
+++ autodiet-frontend/src/Components/AdminHome.tsx
@@ -5,11 +5,14 @@ export const AdminHome = () => {
                 
             </div>
             <div className="flex flex-col w-5/6 h-full">
-                <div className="h-16 w-full bg-white">
-                    <div className=""><img src=""></img></div>
+                <div className="flex h-16 w-full bg-white drop-shadow">
+                    <div className="flex items-center ml-auto self-center w-40 h-10 rounded">
+                        <img className="w-7 h-6 rounded-full mr-1" src="../logo512.png"></img>
+                        <span className="w-1/3 text-md">Admin</span>
+                    </div>
                 </div>
                 <div className="h-screen w-full bg-admin-grey-background">
-
+                    
                 </div>
             </div>
         </div>
