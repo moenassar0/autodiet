c762c1106f0a2d65e3697d1a6e8146ef3d4bd4fd
moenassar0 <mnassar57@gmail.com>
2022-11-04 11:47:00 +0200

    backend: add/update function added for weight entry

--- autodiet-backend/app/Http/Controllers/WeightEntryController.php
+++ autodiet-backend/app/Http/Controllers/WeightEntryController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\WeightEntry;
+use Validator;
 
 class WeightEntryController extends Controller
 {
     public function addOrUpdateWeightEntry(Request $request){
         $entry = WeightEntry::where('user_id', $request->user_id)->where('date', $request->date)->first();
+        if(!$entry) $entry = new WeightEntry;
+        
+        $validator = Validator::make($request->all(),[
+            'weight' => 'required',
+            'date' => 'required|date'
+        ]);
+
+        if($validator->fails()) return response()->json($validator->errors()->toJson(), 400);
+        
+        $entry->fill($validator->validated());
+        $entry->user_id = $request->user_id;
+        $entry->save();    
         return response()->json(['entry:' => $entry], 200);
     }
 }
