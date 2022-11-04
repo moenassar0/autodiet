e2d1c85adeaf9051f45b715b0d7780ef9e2a54a5
moenassar0 <mnassar57@gmail.com>
2022-11-04 09:35:17 +0200

    backend: food item add function implemented

--- autodiet-backend/app/Http/Controllers/FoodItemController.php
+++ autodiet-backend/app/Http/Controllers/FoodItemController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\FoodItem;
+use Validator;
 
 class FoodItemController extends Controller
 {
     public function addFoodItem(Request $request){
         $food_item = new FoodItem;
 
+        /*$food_item->serving_size = $request->serving_size;
+        $food_item->title = $request->title;
+        $food_item->calories = $request->calories;
+        $food_item->protein = $request->protein;
+        $food_item->carbohydrate = $request->carbohydrate;
+        $food_item->fat = $request->fat;
+        $food_item->picture_url = $request->picture_url;*/
+
+        $validator = Validator::make($request->all(),[
+            'serving_size' => 'required',
+            'title' => 'required',
+            'calories' => 'required',
+            'protein' => 'required',
+            'carbohydrate' => 'required',
+            'fat' => 'required',
+            'picture_url' => 'required',
+        ]);
+
+        if($validator->fails()) return response()->json($validator->errors()->toJson(), 400);
+        
+        $food_item->fill($validator->validated());
         $food_item->save();
 
-        return response()->json(["result" => "ok", 'meal added:' => $food_item], 201);
+        return response()->json(["result" => "ok", 'food_item added:' => $food_item], 201);
     }
 }
