901f60735da8e2fbcb1001e11030e630458fa1e8
moenassar0 <mnassar57@gmail.com>
2022-11-05 22:26:19 +0200

    frontend: sidenavbar styling updated

--- autodiet-frontend/src/Components/SideNavbarItem.tsx
+++ autodiet-frontend/src/Components/SideNavbarItem.tsx
@@ -3,8 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const SideNavbarItem: React.FC<SideNavBarItemInterface> = ({title, icon, condition}) => {
     return(
-        <div className={(condition ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
-            <FontAwesomeIcon icon={icon} className={"text-white " + (condition ? "mr-2" : "")} />
+        <div className={(condition ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-center h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-1 py-2"}>
+            <FontAwesomeIcon icon={icon} className={"text-white " + (condition ? "mr-2" : "") + (title==="Logout" ? " hover:text-red-500" : "")} />
             <span className={condition ? "" : "hidden"}> 
                 {title}
             </span>
