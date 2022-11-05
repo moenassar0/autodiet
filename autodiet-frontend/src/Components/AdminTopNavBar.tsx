93f0c16ee56de58eb460eed64fd36f277cead69a
moenassar0 <mnassar57@gmail.com>
2022-11-05 10:59:06 +0200

    frontend:  sliding navbar implemented

--- autodiet-frontend/src/Components/AdminTopNavBar.tsx
+++ autodiet-frontend/src/Components/AdminTopNavBar.tsx
@@ -1,8 +1,25 @@
-export const AdminTopNavBar = () => {
-    return(
-        <div className="flex flex-wrap content-start items-center h-screen w-1/6 bg-admin-main overflow-auto">
-            <div className="flex w-full h-32 bg-black px-2 py-2">
+import { useState } from "react";
+import { UilAlignRight } from '@iconscout/react-unicons'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
 
+
+
+export const AdminTopNavBar = () => {
+
+    const [openNavbar, setOpenNavbar] = useState(true);
+
+    const toggleNavbar = () => {
+        setOpenNavbar(!openNavbar);
+    }
+
+    return(
+        <div style={{width: !openNavbar ? "50px" : "300px"}} className="ease-in duration-150 flex flex-wrap content-start items-center h-screen w-1/6 bg-admin-main overflow-auto">
+            <div className="flex w-full h-24 bg-grey px-2 py-2">
+                <div onClick={toggleNavbar} className={"flex self-start items-center w-8 justify-center h-8 ml-auto cursor-pointer " }>
+                <FontAwesomeIcon icon={faBars} className="text-white" />
+                </div>
             </div>
             <div className="flex w-full h-32 bg-black px-2 py-2">
 
