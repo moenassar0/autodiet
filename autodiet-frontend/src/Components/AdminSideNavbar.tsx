2f3a91357b4f2e319f6f0135c1364ae99d1d9d78
moenassar0 <mnassar57@gmail.com>
2022-11-05 15:40:06 +0200

    frontend: name fixed

--- autodiet-frontend/src/Components/AdminSideNavbar.tsx
+++ autodiet-frontend/src/Components/AdminSideNavbar.tsx
@@ -6,7 +6,7 @@ import { faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser,
 import { SideNavbarItem } from "./SideNavbarItem";
 
 
-export const AdminTopNavBar = () => {
+export const AdminSideNavbar = () => {
 
     const [openNavbar, setOpenNavbar] = useState(true);
 
@@ -15,7 +15,7 @@ export const AdminTopNavBar = () => {
     }
 
     return(
-        <div style={{width: !openNavbar ? "50px" : "300px"}} className="ease-in duration-150 flex flex-wrap content-start items-center h-screen w-1/6 bg-admin-main overflow-auto">
+        <div style={{width:openNavbar ? "250px" : "50px"}}className={(openNavbar ? "" : "") + " ease-in duration-150 flex flex-wrap content-start items-center h-screen bg-admin-main overflow-auto"}>
             <div className="flex w-full h-24 bg-grey px-2 py-2">
                 <div onClick={toggleNavbar} className={"flex self-start items-center justify-center h-8 ml-auto cursor-pointer " + (openNavbar ? "w-8" : "w-full")}>
                     <FontAwesomeIcon icon={faBars} className="text-white" />
