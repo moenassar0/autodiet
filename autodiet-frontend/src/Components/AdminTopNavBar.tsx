72eb374252b5c0a96c69bffcd451d1fc86ae16ee
moenassar0 <mnassar57@gmail.com>
2022-11-05 11:46:04 +0200

    frontend: admin navbar implemented with icons

--- autodiet-frontend/src/Components/AdminTopNavBar.tsx
+++ autodiet-frontend/src/Components/AdminTopNavBar.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { UilAlignRight } from '@iconscout/react-unicons'
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -17,18 +17,39 @@ export const AdminTopNavBar = () => {
     return(
         <div style={{width: !openNavbar ? "50px" : "300px"}} className="ease-in duration-150 flex flex-wrap content-start items-center h-screen w-1/6 bg-admin-main overflow-auto">
             <div className="flex w-full h-24 bg-grey px-2 py-2">
-                <div onClick={toggleNavbar} className={"flex self-start items-center w-8 justify-center h-8 ml-auto cursor-pointer " }>
-                <FontAwesomeIcon icon={faBars} className="text-white" />
+                <div onClick={toggleNavbar} className={"flex self-start items-center justify-center h-8 ml-auto cursor-pointer " + (openNavbar ? "w-8" : "w-full")}>
+                    <FontAwesomeIcon icon={faBars} className="text-white" />
                 </div>
             </div>
-            <div className="flex w-full h-32 bg-black px-2 py-2">
-
+            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
+                <FontAwesomeIcon icon={faUser} className={"text-white " + (openNavbar ? "mr-2" : "")} />
+                <span className={openNavbar ? "" : "hidden"}> 
+                    Users
+                </span>
             </div>
-            <div className="flex w-full h-32 bg-black px-2 py-2">
-
+            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
+                <FontAwesomeIcon icon={faPlateWheat} className={"text-white " + (openNavbar ? "mr-2" : "")} />
+                <span className={openNavbar ? "" : "hidden"}> 
+                    Meals
+                </span>
             </div>
-            <div className="flex w-full h-32 bg-black px-2 py-2">
-
+            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
+                <FontAwesomeIcon icon={faDrumstickBite} className={"text-white " + (openNavbar ? "mr-2" : "")} />
+                <span className={openNavbar ? "" : "hidden"}> 
+                    Foods
+                </span>
+            </div>
+            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
+                <FontAwesomeIcon icon={faChartLine} className={"text-white " + (openNavbar ? "mr-2" : "")} />
+                <span className={openNavbar ? "" : "hidden"}> 
+                    Graphs
+                </span>
+            </div>
+            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2 place-self-end"}>
+                <FontAwesomeIcon icon={faRightFromBracket} className={"text-white " + (openNavbar ? "mr-2" : "")} />
+                <span className={openNavbar ? "" : "hidden"}> 
+                    Logout
+                </span>
             </div>
         </div>
     )
