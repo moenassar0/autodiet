3ad9b34362ce2b195d13591590a5496323b608de
moenassar0 <mnassar57@gmail.com>
2022-11-05 13:42:17 +0200

    frontend: changed anavbar into components

--- autodiet-frontend/src/Components/AdminTopNavBar.tsx
+++ autodiet-frontend/src/Components/AdminTopNavBar.tsx
@@ -3,7 +3,7 @@ import { UilAlignRight } from '@iconscout/react-unicons'
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
-
+import { SideNavbarItem } from "./SideNavbarItem";
 
 
 export const AdminTopNavBar = () => {
@@ -21,36 +21,11 @@ export const AdminTopNavBar = () => {
                     <FontAwesomeIcon icon={faBars} className="text-white" />
                 </div>
             </div>
-            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
-                <FontAwesomeIcon icon={faUser} className={"text-white " + (openNavbar ? "mr-2" : "")} />
-                <span className={openNavbar ? "" : "hidden"}> 
-                    Users
-                </span>
-            </div>
-            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
-                <FontAwesomeIcon icon={faPlateWheat} className={"text-white " + (openNavbar ? "mr-2" : "")} />
-                <span className={openNavbar ? "" : "hidden"}> 
-                    Meals
-                </span>
-            </div>
-            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
-                <FontAwesomeIcon icon={faDrumstickBite} className={"text-white " + (openNavbar ? "mr-2" : "")} />
-                <span className={openNavbar ? "" : "hidden"}> 
-                    Foods
-                </span>
-            </div>
-            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2"}>
-                <FontAwesomeIcon icon={faChartLine} className={"text-white " + (openNavbar ? "mr-2" : "")} />
-                <span className={openNavbar ? "" : "hidden"}> 
-                    Graphs
-                </span>
-            </div>
-            <div className={(openNavbar ? "w-5/6" : "justify-center w-full") + " rounded-r-full flex items-center justify-start h-12 hover:bg-admin-hoverednav text-white hover:text-admin-hoveredtext cursor-pointer px-2 py-2 place-self-end"}>
-                <FontAwesomeIcon icon={faRightFromBracket} className={"text-white " + (openNavbar ? "mr-2" : "")} />
-                <span className={openNavbar ? "" : "hidden"}> 
-                    Logout
-                </span>
-            </div>
+            <SideNavbarItem condition={openNavbar} icon={faUser} title="Users"></SideNavbarItem>
+            <SideNavbarItem condition={openNavbar} icon={faPlateWheat} title="Meals"></SideNavbarItem>
+            <SideNavbarItem condition={openNavbar} icon={faDrumstickBite} title="Foods"></SideNavbarItem>
+            <SideNavbarItem condition={openNavbar} icon={faChartLine} title="Graphs"></SideNavbarItem>
+            <SideNavbarItem condition={openNavbar} icon={faRightFromBracket} title="Logout"></SideNavbarItem>
         </div>
     )
 }
