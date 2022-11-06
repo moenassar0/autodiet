cd8474656bfc77b06fc448c4cec1890d3c4ccaec
moenassar0 <mnassar57@gmail.com>
2022-11-06 10:41:12 +0200

    frontend: responsitivity styling updated

--- autodiet-frontend/src/Components/AdminSideNavbar.tsx
+++ autodiet-frontend/src/Components/AdminSideNavbar.tsx
@@ -15,7 +15,7 @@ export const AdminSideNavbar = () => {
     }
 
     return(
-        <div style={{width:openNavbar ? "250px" : "50px"}}className={(openNavbar ? "" : "") + " ease-in duration-150 flex flex-wrap content-start items-center h-screen bg-admin-main overflow-auto"}>
+        <div className={(openNavbar ? "w-2/6 sm:w-1/6" : "w-10") + " ease-in duration-150 flex flex-wrap content-start items-center h-screen bg-admin-main overflow-auto"}>
             <div className="flex w-full h-24 bg-grey px-2 py-2">
                 <div onClick={toggleNavbar} className={"flex self-start items-center justify-center h-8 ml-auto cursor-pointer " + (openNavbar ? "w-8" : "w-full")}>
                     <FontAwesomeIcon icon={faBars} className="text-white" />
