1e7e2c8280929436495faa2c09fa276f90a448e0
moenassar0 <mnassar57@gmail.com>
2022-11-06 10:14:53 +0200

    backend: updated route api function

--- autodiet-backend/routes/api.php
+++ autodiet-backend/routes/api.php
@@ -15,7 +15,7 @@ Route::post("/login", [AuthController::class, "login"]);
 Route::get("/me", [AuthController::class, "me"]);
 
 Route::middleware([IsUser::class])->group(function () {
-    Route::get("/meals", [MealController::class, "getMeals"]);
+    Route::get("/meals", [MealController::class, "getCustomizedMeals"]);
     Route::get("/meal/{id}", [MealController::class, "getMealRecipe"]);
     Route::get("/meals/{search_string}", [MealController::class, "getMealsByTitle"]);
     Route::get("/user/{id}", [UserController::class, "getUserDetail"]);
