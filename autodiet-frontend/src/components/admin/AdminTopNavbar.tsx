b0b6f1488591ddb0a799cf08bffb44149228cf71
moenassar0 <mnassar57@gmail.com>
2022-11-07 01:20:11 +0200

    frontend: admin navbar updated

--- autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -1,8 +1,14 @@
+import { faLightbulb } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 export const AdminTopNavbar = () => {
     return(
-        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow">
-            <div className="flex items-center ml-auto self-center w-40 h-10 rounded">
-                <img className="w-7 h-6 rounded-full mr-1" src="../logo512.png"></img>
+        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow justify-end">
+            <div className="flex items-center justify-center self-center bg-admin-grey-background drop-shadow h-9 w-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
+            <FontAwesomeIcon icon={faLightbulb} className="text-blue" />
+            </div>
+            <div className="flex items-center self-center w-40 h-10 rounded">
+                <img className="w-6 h-6 rounded-full mr-1" src="../logo512.png"></img>
                 <span className="w-1/3 text-md">Admin</span>
             </div>
         </div>
