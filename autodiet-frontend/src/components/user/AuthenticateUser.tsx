7914130c48999961c122c14e84ab9814093155f6
moenassar0 <mnassar57@gmail.com>
2022-11-06 22:25:52 +0200

    frontend: authenticate user implemented

--- autodiet-frontend/src/components/user/AuthenticateUser.tsx
+++ autodiet-frontend/src/components/user/AuthenticateUser.tsx
@@ -1,7 +1,18 @@
 import { Outlet } from "react-router-dom"
 import { validateUser, } from "../../HelperFunctions";
 import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
+import { useEffect } from "react";
 export const AuthenticateUser = () => {
+    
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        //If user doesn't have a valid token navigate him back to landing page
+        const check = async () => {if(! await validateUser()){
+            navigate("/");
+        }}
+        check();
+    }, [])
 
     return(
         <Outlet />
