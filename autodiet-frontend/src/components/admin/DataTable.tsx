e10093d11c3ddfda0599ed3e1c83f7e9aa61b80a
moenassar0 <mnassar57@gmail.com>
2022-11-07 01:02:35 +0200

    frontend: routes updated

--- autodiet-frontend/src/components/admin/DataTable.tsx
+++ autodiet-frontend/src/components/admin/DataTable.tsx
@@ -1,37 +1,45 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
-import { UserInterface } from '../../types/types'
+import { UserInterface, UserHeaders } from '../../types/types'
 import { Key } from 'react';
 
-export const DataTable: React.FC<{data:any}> = ({data}) => {
-    const user = data[0];
-    let headers:Array<string> = [];
-    if(user){
-        headers = (Object.keys(user));
-        console.log(headers);
-    }
-    
-    //const headers2:any = Object.keys(headers)
+export const DataTable: React.FC<{data:Array<Object>, headers:Array<string>}> = ({data, headers}) => {
+    //Scalable data table for CRUD operations.
+    //Headers are keys in the data object
+    //Headers will be mapped as table headers and then the data array will be mapped using the headers
     return(          
             <div className="h-auto w-full shadow-sm">
                 <table className="border-collapse table-auto w-full text-sm">
                     <thead className="text-center bg-[#F5F5F6]">
                         <tr>
-                            {headers?.map((header) => (
-                                <th className="border-b dark:border-slate-600 font-medium text-slate-400 py-2 pl-2 dark:text-slate-200 text-left">
-                                    {header}
+                            {headers?.map((header, i) => (
+                                <th key={i} className="border-b dark:border-slate-600 font-medium text-slate-400 py-2 pl-8 dark:text-slate-200 text-left">
+                                    {header.toUpperCase()}
                                 </th>
                             ))}
+                            <th className="border-b dark:border-slate-600 font-medium text-slate-400 py-2 pl-8 dark:text-slate-200 text-left">
+                                ACTIONS
+                            </th>
                         </tr>
                     </thead>
                     <tbody className="w-60 bg-white dark:bg-slate-800">
-                        {data.map((user:any) => (
-                            <tr>
-                            {headers?.map((header) => (
-                                <td className="border-b dark:border-slate-600 font-medium text-slate-400 py-2 pl-2 dark:text-slate-200 text-left">
-                                    {user[header]}
+                        {data.map((data:any, i) => (
+                            <tr key={data.id}>
+                            {headers?.map((header, i) => (
+                                <td key={i} className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">
+                                    {data[header]}
                                 </td>
                             ))}
+                                <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
+                                    <button onClick={() => {console.log(data.id)}} className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>
+                                        Edit
+                                        <FontAwesomeIcon className="self-center ml-2" icon={faEdit}/>
+                                    </button>
+                                    <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>
+                                        Delete
+                                        <FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/>
+                                    </button>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
@@ -39,17 +47,3 @@ export const DataTable: React.FC<{data:any}> = ({data}) => {
             </div>
     )
 }
-
-/*
-            {users.map((datum:any, index:number) => {
-              return (
-                <tr key={datum.id}>
-                  {headers2.map((header:any, index: Key | null | undefined) => {
-                      <td key={index}>
-                          <span>{datum[header]}</span>                      
-                      </td>
-                  })}
-                </tr>
-              );
-            })}
-*/
