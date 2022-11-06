41c57679840b315da8781fb85d6ea3a152682eaa
moenassar0 <mnassar57@gmail.com>
2022-11-07 00:23:40 +0200

    frontend: topnavbar component base added

---
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -0,0 +1,10 @@
+export const AdminTopNavbar = () => {
+    return(
+        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow">
+            <div className="flex items-center ml-auto self-center w-40 h-10 rounded">
+                <img className="w-7 h-6 rounded-full mr-1" src="../logo512.png"></img>
+                <span className="w-1/3 text-md">Admin</span>
+            </div>
+        </div>
+    )
+}
