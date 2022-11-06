0cf8f4d697297d0d5818afe768e2a65d920ff26f
moenassar0 <mnassar57@gmail.com>
2022-11-07 01:01:01 +0200

    frontend: sidenavbar items updated with route paths

--- autodiet-frontend/src/components/admin/AdminSideNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminSideNavbar.tsx
@@ -4,7 +4,7 @@ import UilReact from '@iconscout/react-unicons/icons/uil-react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
 import { SideNavbarItem } from "./SideNavbarItem";
-
+import { Link } from "react-router-dom"
 
 export const AdminSideNavbar = () => {
 
@@ -21,11 +21,11 @@ export const AdminSideNavbar = () => {
                     <FontAwesomeIcon icon={faBars} className="text-white" />
                 </div>
             </div>
-            <SideNavbarItem condition={openNavbar} icon={faUser} title="Users"></SideNavbarItem>
-            <SideNavbarItem condition={openNavbar} icon={faPlateWheat} title="Meals"></SideNavbarItem>
-            <SideNavbarItem condition={openNavbar} icon={faDrumstickBite} title="Foods"></SideNavbarItem>
-            <SideNavbarItem condition={openNavbar} icon={faChartLine} title="Graphs"></SideNavbarItem>
-            <SideNavbarItem condition={openNavbar} icon={faRightFromBracket} title="Logout"></SideNavbarItem>
+            <SideNavbarItem path="/admin/users" condition={openNavbar} icon={faUser} title="Users"></SideNavbarItem>
+            <SideNavbarItem path="/admin/meals" condition={openNavbar} icon={faPlateWheat} title="Meals"></SideNavbarItem>
+            <SideNavbarItem path="/admin/food" condition={openNavbar} icon={faDrumstickBite} title="Foods"></SideNavbarItem>
+            <SideNavbarItem path="/admin/users" condition={openNavbar} icon={faChartLine} title="Graphs"></SideNavbarItem>
+            <SideNavbarItem path="/admin/users" condition={openNavbar} icon={faRightFromBracket} title="Logout"></SideNavbarItem>
         </div>
     )
 }
