4c965f8fc89a869ee992fef81f0ce507f934340a
moenassar0 <mnassar57@gmail.com>
2022-11-07 00:23:27 +0200

    frontend: admin topnav imported as component

--- autodiet-frontend/src/pages/admin/AdminHome.tsx
+++ autodiet-frontend/src/pages/admin/AdminHome.tsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react"
 import { AdminSideNavbar } from "../../components/admin/AdminSideNavbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
-import { DataTable } from "../../components/admin/DataTable";
+import { DataTable } from "../../components/admin/DataTable"
 import { firebase_init } from "../../api/firebase_init_test";
 import axios from "axios";
 import { sendNotification } from "../../HelperFunctions";
 import { getUsers } from "../../api/services/Users";
-import { UserInterface } from "../../types/types";
+import { UserInterface, UserHeaders } from '../../types/types'
+import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar";
 
 export const AdminHome = () => {
 
@@ -31,13 +32,7 @@ export const AdminHome = () => {
         <div className="flex h-screen w-full">
             <AdminSideNavbar/>
             <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
-                {/*<!-- Topnavbar -->*/}
-                <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow">
-                    <div className="flex items-center ml-auto self-center w-40 h-10 rounded">
-                        <img className="w-7 h-6 rounded-full mr-1" src="../logo512.png"></img>
-                        <span className="w-1/3 text-md">Admin</span>
-                    </div>
-                </div>
+                <AdminTopNavbar />
                 <div className="h-5/6 grow w-full bg-admin-grey-background px-4 py-4">
                     <div className="flex items-center justify-start h-12 w-full rounded-t bg-white px-3 pl-8">
                         <span className="text-lg font-semibold">Users</span>
@@ -47,7 +42,7 @@ export const AdminHome = () => {
                     </div>
                     <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
                         <div className="flex flex-col w-full h-auto">
-                            <DataTable users={users}></DataTable>
+                            <DataTable data={users} headers={UserHeaders}></DataTable>
                         </div>
                     </div>
                 </div>
