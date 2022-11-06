07389b0168dd0a0852f78e9a850bc0aabd7b612a
moenassar0 <mnassar57@gmail.com>
2022-11-06 22:57:07 +0200

    frontend: fetched users displayed in table

--- autodiet-frontend/src/components/admin/DataTable.tsx
+++ autodiet-frontend/src/components/admin/DataTable.tsx
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { UserInterface } from '../../types/types'
 
-export const DataTable = () => {
+export const DataTable: React.FC<{users:Array<UserInterface>}> = ({users}) => {
     return(          
             <div className="h-auto w-full shadow-sm">
                 <table className="border-collapse table-auto w-full text-sm">
@@ -13,134 +14,16 @@ export const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody className="w-60 bg-white dark:bg-slate-800">
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
+                {users.map((user: UserInterface) => (
+                    <tr key={user.id}>
+                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">{user.username}</td>
+                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">{user.email}</td>
                         <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
                             <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
                             <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
                         </td>
                     </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-400 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-400 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-400 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-400 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-700 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-8 dark:text-slate-400">croozeraid</td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">mnassar@gmail.com</td>
-                        <td className="flex gap-2 border-b border-slate-100 dark:border-slate-700 text-slate-500 py-2 pl-2 dark:text-slate-400">
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Edit User<FontAwesomeIcon className="self-center ml-2" icon={faEdit}/></button>
-                            <button className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton rounded-full px-1 py-1 font-medium'>Delete User<FontAwesomeIcon className="text-red-400 self-center ml-2" icon={faTrash}/></button>
-                        </td>
-                    </tr>
+                ))}
                 </tbody>
             </table>
         </div>
