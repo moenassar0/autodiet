18d706c3c232e18afe40c9440773a32f806abdc8
moenassar0 <mnassar57@gmail.com>
2022-11-07 19:35:16 +0200

    frontend: api base function code updated

--- autodiet-frontend/src/api/services/baseAPICall.ts
+++ autodiet-frontend/src/api/services/baseAPICall.ts
@@ -1,26 +1,20 @@
 import { getToken } from "../../HelperFunctions";
 import axios from "../axios";
 
-export const baseAPICall = async (url: string, params: any, callMethod: string) => {
-    if(!params) params = {};
+export const get = async (url: string, parameter: string) => {
+    try{
+        const response = await axios.get(url, getToken());
+        if(response.data) return {success: true, response: response.data};
+    }catch(err){
+        return {success: false, response: err};
+    }
+}
 
-    switch(callMethod){
-        case("get"):
-            try{
-                const response = await axios.get(url, getToken());
-                if(response.data) return {success: true, response: response.data};
-            }catch(err){
-                return {success: false, response: err};
-            }
-            break;
-
-        case("post"):
-            try{
-                const response = await axios.post(url, params, getToken());
-                if(response.data) return {success: true, response: response.data};
-            }catch(err){
-                return {success: false, response: err};
-            }
-            break;
+export const post = async (url: string, data: object) => {
+    try{
+        const response = await axios.post(url, data, getToken());
+        if(response.data) return {success: true, response: response.data};
+    }catch(err){
+        return {success: false, response: err};
     }
 }
