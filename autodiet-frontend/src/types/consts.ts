05f749ab7d516341a3f18eba1bd4b36dde33adbc
moenassar0 <mnassar57@gmail.com>
2022-11-07 18:06:36 +0200

    frontend: project consts file added

---
+++ autodiet-frontend/src/types/consts.ts
@@ -0,0 +1,18 @@
+import { faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser } from '@fortawesome/free-solid-svg-icons'
+
+export const userNavbarLinks = [
+    { path: "/user/home", icon: faUser, title: "Users" },
+    { path: "/user/profile", icon: faPlateWheat, title: "Meals" },
+    { path: "/user/meals", icon: faDrumstickBite, title: "Foods" },
+    { path: "/user/foods", icon: faChartLine, title: "Graphs" },
+    { path: "/user/weight", icon: faRightFromBracket, title: "Logout" },
+];
+
+//Consts for admin pages
+export const navbarlinks = [
+    { path: "/admin/users", icon: faUser, title: "Users" },
+    { path: "/admin/meals", icon: faPlateWheat, title: "Meals" },
+    { path: "/admin/food", icon: faDrumstickBite, title: "Foods" },
+    { path: "/admin/users", icon: faChartLine, title: "Graphs" },
+    { path: "/admin/users", icon: faRightFromBracket, title: "Logout" },
+];
