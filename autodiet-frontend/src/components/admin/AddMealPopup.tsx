02f89b70cb98449ade2c4b2c4b6a865def29159a
moenassar0 <mnassar57@gmail.com>
2022-11-07 21:26:21 +0200

    frontend: add meal function implemented

--- autodiet-frontend/src/components/admin/AddMealPopup.tsx
+++ autodiet-frontend/src/components/admin/AddMealPopup.tsx
@@ -3,13 +3,16 @@ import { addUser } from "../../api/services/Users"
 import { useState } from "react"
 import { InputFieldInterface } from "../../types/types"
 import { Popup } from "../utility/Popup"
+import { addMeal } from "../../api/services/Meals"
 
 export const AddMealPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({setTrigger, trigger}) => {
 
     const [title, setTitle] = useState("");
     const [calories, setCalories] = useState(0);
     const [protein, setProtein] = useState(0);
-    const [carbohydrates, setCarbohydrates] = useState(0);
+    const [carbohydrate, setCarbohydrate] = useState(0);
+
+    const [message, setMessage] = useState("");
 
     const inputs: Array<InputFieldInterface> = [
         {
@@ -22,22 +25,22 @@ export const AddMealPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({set
             title:"Protein", error:"asd", setHook:setProtein, state:protein.toString(), valid:true
         },
         {
-            title:"Carbohydrate", error:"asd", setHook:setCarbohydrates, state:carbohydrates.toString(), valid:true
+            title:"Carbohydrate", error:"asd", setHook:setCarbohydrate, state:carbohydrate.toString(), valid:true
         },
     ];
 
-    /*const handleSubmit = async () => {
-        const response = await addUser({username, email, password, user_role: "User"});
+    const handleSubmit = async () => {
+        const response = await addMeal({title, calories, type:"Snack", protein, carbohydrate, fat:50, protein_percentage:5, picture_url:"asd"});
         if(!response?.success) setMessage("Server Error");
         else setMessage("User added!");
-    }*/
+    }
 
     return(
         <section>
            {trigger ? 
             (   <>
                 <PopupOverlay></PopupOverlay>
-                <Popup title="Add Meal" message={""} inputs={inputs} submitMethod={() => {console.log("gg")}}/>
+                <Popup title="Add Meal" message={message} inputs={inputs} submitMethod={handleSubmit} />
                 </>
             ) : ("") } 
         </section>
