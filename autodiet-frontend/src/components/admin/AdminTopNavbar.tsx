1ea2a18a95d65367b7c29bc4b1495255c6a87858
moenassar0 <mnassar57@gmail.com>
2022-11-07 19:34:48 +0200

    frontend: navbar styling updated

--- autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -27,12 +27,12 @@ export const AdminTopNavbar:React.FC<{title: string, username: string, children:
     }, [theme])
 
     return(
-        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
+        <div className="flex gap-4 h-1/6 overflow-x-scroll max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
             <span className="flex items-center pl-6 font-medium text-2xl dark:text-ad-golden text-admin-main">{title}</span>
-            {children}
-            <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="ml-auto flex items-center justify-center self-center bg-admin-grey-background dark:text-black dark:bg-ad-golden drop-shadow h-9 w-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
-                <FontAwesomeIcon icon={faLightbulb} className="text-blue" />
+            <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="ml-auto flex items-center justify-center self-center bg-admin-grey-background dark:text-black dark:bg-ad-golden drop-shadow h-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
+                <FontAwesomeIcon icon={faLightbulb} className="w-10 text-blue" />
             </div>
+            {children}
             <div className="flex items-center self-center w-40 h-10 rounded">
                 <img className="w-6 h-6 rounded-full mr-2" src="../logo512.png"></img>
                 <span className="w-1/3 text-md dark:text-slate-200">{username}</span>
--- autodiet-frontend/src/components/utility/Button.tsx
+++ autodiet-frontend/src/components/utility/Button.tsx
@@ -1,6 +1,6 @@
 export const Button: React.FC<{title: string, onclickMethod: any}> = ({title, onclickMethod}) => {
     return(
-        <button onClick={onclickMethod} className='flex w-28 justify-center h-2/3 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium'>
+        <button onClick={onclickMethod} className='flex w-28 justify-center items-center h-10 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium'>
             {title}
         </button>
     )
