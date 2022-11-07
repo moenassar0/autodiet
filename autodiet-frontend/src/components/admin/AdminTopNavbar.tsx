e2d227b5a41551a4d0b7efba600fe8a317ca3b71
moenassar0 <mnassar57@gmail.com>
2022-11-07 12:34:22 +0200

    frontend: dark mode switch implemented

--- autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -1,11 +1,34 @@
 import { faLightbulb } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useEffect, useState } from "react"
 
 export const AdminTopNavbar = () => {
+
+    const [theme, setTheme] = useState("light");
+
+    useEffect(() => {
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+          setTheme('dark');
+        }
+        else {
+          setTheme('light');
+        }
+      }, [])
+
+    useEffect(() => {
+        
+        if(theme === "dark"){
+            document.documentElement.classList.add("dark");
+            console.log(theme);
+        }else{
+            document.documentElement.classList.remove("dark");
+        }
+    }, [theme])
+
     return(
-        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow justify-end">
-            <div className="flex items-center justify-center self-center bg-admin-grey-background drop-shadow h-9 w-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
-            <FontAwesomeIcon icon={faLightbulb} className="text-blue" />
+        <div className="flex h-1/6 max-h-16 w-full bg-white drop-shadow justify-end dark:bg-admin-dark-topnav">
+            <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="flex items-center justify-center self-center bg-admin-grey-background drop-shadow h-9 w-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
+                <FontAwesomeIcon icon={faLightbulb} className="text-blue" />
             </div>
             <div className="flex items-center self-center w-40 h-10 rounded">
                 <img className="w-6 h-6 rounded-full mr-1" src="../logo512.png"></img>
