32f94353183fcea057bf096f16feb268f9cd929f
moenassar0 <mnassar57@gmail.com>
2022-11-07 14:56:00 +0200

    frontend: input field added to popup

--- autodiet-frontend/src/components/utility/InputField.tsx
+++ autodiet-frontend/src/components/utility/InputField.tsx
@@ -6,12 +6,12 @@ const InputField: React.FC<InputFieldInterface> = ({title, error, state, setHook
 
     return(
         <>
-            <div className="flex items-center place-content-between w-full h-10">
-                <span className="w-2/5 text-ad-golden text-lg">{title}</span>
+            <div className="flex items-center place-content-between w-full h-10 px-2 py-1">
+                <span className="w-2/5 text-admin-main dark:text-ad-golden text-lg">{title}</span>
                 <input 
                     onChange={(e) => setHook(e.target.value)} 
-                    className={"w-3/5 py-2 px-4 text-white bg-black rounded focus:outline-none focus:ring-2 focus:border-ad-golden focus:ring-ad-golden" +
-                    (!valid && state ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="password">
+                    className={"h-full w-3/5 py-2 px-4 bg-admin-main text-white dark:bg-black rounded focus:outline-none focus:ring-2 focus:border-admin-button focus:ring-admin-button dark:focus:border-ad-golden dark:focus:ring-ad-golden" +
+                    (!valid && state ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="text">
                 </input>
             </div>
             {!valid && state ? <p className="w-3/5 flex self-end text-red-600">{error}</p> : ""}
