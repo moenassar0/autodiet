9bec4dad9c84711234d97ffee9698b648e19d089
moenassar0 <mnassar57@gmail.com>
2022-11-07 14:00:43 +0200

    frontend: popup component added

--- autodiet-frontend/src/pages/admin/AdminUsers.tsx
+++ autodiet-frontend/src/pages/admin/AdminUsers.tsx
@@ -10,10 +10,12 @@ import { getUsers } from "../../api/services/Users";
 import { UserInterface, UserHeaders } from '../../types/types'
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar";
 import { DarkModeTest } from "./DarkModeTest";
+import { AddUserPopup } from "../../components/admin/AddUserPopup";
 
 export const AdminUsers = () => {
 
     const [users, setUsers] = useState([]);
+    const [addUsersPopup ,setAddUsersPopup] = useState(false);
     
     async function fetchUsers(){
         try{
@@ -31,14 +33,15 @@ export const AdminUsers = () => {
 
     return(
         <div className="flex h-screen w-full">
+            <AddUserPopup trigger={addUsersPopup} setTrigger={setAddUsersPopup}/>
             <AdminSideNavbar/>
             <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
                 <AdminTopNavbar />
                 <div className="h-5/6 grow w-full bg-admin-grey-background px-4 py-4 dark:bg-[#1F1F1F]">
                     <div className="flex items-center justify-start h-12 w-full rounded-t bg-white px-3 pl-8 dark:bg-admin-dark-background dark:text-slate-200">
                         <span className="text-lg font-semibold">Users</span>
-                        <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton rounded-full px-1 py-1">
-                            <button>Add User<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
+                        <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1">
+                            <button onClick={() => {setAddUsersPopup(true)}}>Add User<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
                         </div>
                     </div>
                     <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
