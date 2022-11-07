acb228aea8045c2b9b9185e79772f2e0b8d4abf2
moenassar0 <mnassar57@gmail.com>
2022-11-07 20:49:31 +0200

    frontend: popup as component added

--- autodiet-frontend/src/components/admin/AddUserPopup.tsx
+++ autodiet-frontend/src/components/admin/AddUserPopup.tsx
@@ -2,6 +2,8 @@ import { PopupOverlay } from "../utility/PopupOverlay"
 import InputField from "../../components/utility/InputField"
 import { addUser } from "../../api/services/Users"
 import { useState } from "react"
+import { InputFieldInterface } from "../../types/types"
+import { Popup } from "../utility/Popup"
 
 export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({setTrigger, trigger}) => {
 
@@ -9,6 +11,18 @@ export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({set
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
+
+    const inputs: Array<InputFieldInterface> = [
+        {
+            title:"Username", error:"asd", setHook:setUsername, state:username, valid:true
+        },
+        {
+            title:"Email", error:"asd", setHook:setEmail, state:email, valid:true
+        },
+        {
+            title:"Password", error:"asd", setHook:setPassword, state:password, valid:true
+        },
+    ];
 
     const handleSubmit = async () => {
         const response = await addUser({username, email, password, user_role: "User"});
@@ -21,28 +35,10 @@ export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({set
            {trigger ? 
             (   <>
                 <PopupOverlay></PopupOverlay>
-                <div className="absolute flex w-full h-screen z-20 items-center justify-center">
-                    <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-y-scroll gap-2 rounded-t-lg">
-                        <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
-                            <span>Add User {message ? message : ""}</span>
-                        </div>
-                        <InputField title="Username" error="asd" setHook={setUsername} state={username} valid={true}></InputField>
-                        <InputField title="Email" error="asd" setHook={setEmail} state={email} valid={true}></InputField>
-                        <InputField title="Password" error="asd" setHook={setPassword} state={password} valid={true}></InputField>
-                        <div className="flex w-full h-full justify-end items-center py-2 px-2">
-                            <button onClick={handleSubmit} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">Add</button>
-                        </div>
-                    </div>
-                </div>
+                <Popup title="Add User" message={message} inputs={inputs} submitMethod={handleSubmit}/>
                 </>
             ) : ("") } 
         </section>
     )
 
 }
-
-/*
-                <div className="w-48 sm:flex top-5 absolute left-0 right-0 ml-auto mr-auto w-48 h-96 h-max-screen bg-white z-20">
-
-                </div>
-*/
