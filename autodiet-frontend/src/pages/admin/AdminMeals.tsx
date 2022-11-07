abaa4779d64d682fa24a6dfcd1f2ae951000d328
moenassar0 <mnassar57@gmail.com>
2022-11-07 21:17:03 +0200

    frontend: add meal popup implemented

--- autodiet-frontend/src/pages/admin/AdminMeals.tsx
+++ autodiet-frontend/src/pages/admin/AdminMeals.tsx
@@ -7,10 +7,12 @@ import { DataTable } from "../../components/admin/DataTable"
 import { getMeals } from "../../api/services/Meals";
 import { MealHeaders } from "../../types/types"
 import { adminNavbarLinks } from "../../types/consts"
+import { AddMealPopup } from "../../components/admin/AddMealPopup"
 
 export const AdminMeals = () => {
 
     const [meals, setMeals] = useState([]);
+    const [addMealsPopup ,setAddMealsPopup] = useState(false);
     
     async function fetchMeals(){
         const response = await getMeals();
@@ -24,6 +26,7 @@ export const AdminMeals = () => {
 
     return(
             <div className="flex h-screen w-full">
+                <AddMealPopup trigger={addMealsPopup} setTrigger={setAddMealsPopup} />
                 <SideNavbar navbarlinks={adminNavbarLinks}/>
                 <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
                     <AdminTopNavbar title="" username="Admin">
@@ -33,7 +36,7 @@ export const AdminMeals = () => {
                         <div className="flex items-center justify-start h-12 w-full rounded-t bg-white dark:bg-admin-dark-background dark:text-ad-golden px-3 pl-8">
                             <span className="text-lg font-semibold dark:text-ad-golden">Meals</span>
                             <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:bg-ad-golden dark:text-black rounded-full px-1 py-1">
-                                <button>Add Meal<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
+                                <button onClick={() => {setAddMealsPopup(true)}}>Add Meal<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
                             </div>
                         </div>
                         <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
