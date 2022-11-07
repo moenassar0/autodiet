ce56cb1b9bab0f219c05ce377febd4e196b358f9
moenassar0 <mnassar57@gmail.com>
2022-11-07 14:56:11 +0200

    frontend: popup implemented

--- autodiet-frontend/src/components/admin/AddUserPopup.tsx
+++ autodiet-frontend/src/components/admin/AddUserPopup.tsx
@@ -1,4 +1,5 @@
 import { PopupOverlay } from "../utility/PopupOverlay"
+import InputField from "../../components/utility/InputField"
 
 export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({setTrigger, trigger}) => {
 
@@ -8,17 +9,20 @@ export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({set
             (   <>
                 <PopupOverlay></PopupOverlay>
                 <div className="absolute flex w-full h-screen z-20 items-center justify-center">
-                    <div className="w-4/5 sm:w-4/6 h-5/6 flex flex-wrap bg-white overflow-y-scroll rounded-t-lg">
-                        <div className="flex w-full h-12 text-lg bg-ad-golden items-center px-2 rounded-t-lg border-b border-black">
+                    <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-y-scroll gap-2 rounded-t-lg">
+                        <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
                             <span>Add User</span>
+                        </div>
+                        <InputField title="Username" error="asd" setHook={null} state="" valid={true}></InputField>
+                        <InputField title="Email" error="asd" setHook={null} state="" valid={true}></InputField>
+                        <InputField title="Password" error="asd" setHook={null} state="" valid={true}></InputField>
+                        <div className="flex w-full h-full justify-end items-center py-2 px-2">
+                            <button className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">Add</button>
                         </div>
                     </div>
                 </div>
                 </>
-            )
-            : 
-            ("")
-            } 
+            ) : ("") } 
         </section>
     )
 
