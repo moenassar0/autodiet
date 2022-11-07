a74092f50290de5aefffe9df98bf8bfc41f2697f
moenassar0 <mnassar57@gmail.com>
2022-11-07 16:05:25 +0200

    frontend: add user popup implemented

--- autodiet-frontend/src/components/admin/AddUserPopup.tsx
+++ autodiet-frontend/src/components/admin/AddUserPopup.tsx
@@ -1,7 +1,20 @@
 import { PopupOverlay } from "../utility/PopupOverlay"
 import InputField from "../../components/utility/InputField"
+import { addUser } from "../../api/services/Users"
+import { useState } from "react"
 
 export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({setTrigger, trigger}) => {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = async () => {
+        const response = await addUser({username, email, password, user_role: "User"});
+        if(!response?.success) setMessage("Server Error");
+        else setMessage("User added!");
+    }
 
     return(
         <section>
@@ -11,13 +24,13 @@ export const AddUserPopup: React.FC<{setTrigger: any, trigger: boolean}> = ({set
                 <div className="absolute flex w-full h-screen z-20 items-center justify-center">
                     <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-y-scroll gap-2 rounded-t-lg">
                         <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
-                            <span>Add User</span>
+                            <span>Add User {message ? message : ""}</span>
                         </div>
-                        <InputField title="Username" error="asd" setHook={null} state="" valid={true}></InputField>
-                        <InputField title="Email" error="asd" setHook={null} state="" valid={true}></InputField>
-                        <InputField title="Password" error="asd" setHook={null} state="" valid={true}></InputField>
+                        <InputField title="Username" error="asd" setHook={setUsername} state={username} valid={true}></InputField>
+                        <InputField title="Email" error="asd" setHook={setEmail} state={email} valid={true}></InputField>
+                        <InputField title="Password" error="asd" setHook={setPassword} state={password} valid={true}></InputField>
                         <div className="flex w-full h-full justify-end items-center py-2 px-2">
-                            <button className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">Add</button>
+                            <button onClick={handleSubmit} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">Add</button>
                         </div>
                     </div>
                 </div>
