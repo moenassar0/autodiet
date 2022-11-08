336d1741cecb4f1e2a5804158206d99fbc7ef3c5
moenassar0 <mnassar57@gmail.com>
2022-11-08 15:26:19 +0200

    frontend: line graph base added

--- autodiet-frontend/src/components/charts/DoughnutChart.tsx
+++ autodiet-frontend/src/components/charts/DoughnutChart.tsx
@@ -1,9 +1,12 @@
-import { Doughnut } from "react-chartjs-2"
-import {Chart, ArcElement} from 'chart.js'
-Chart.register(ArcElement);
+import { Doughnut, Line} from "react-chartjs-2"
+import {Chart, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title} from 'chart.js'
+Chart.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title);
 
 export const DoughnutChart = () => {
-    const data = {
+
+    
+
+    const data2 = {
         labels: [
             "20/11/2015", "21/11/2015", "22/11/2015"
         ],
@@ -13,9 +16,21 @@ export const DoughnutChart = () => {
             }
         ]
     }
+    const labels =  ["20/11/2015", "21/11/2015", "22/11/2015"]
+    const data = {
+
+        labels: labels,
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      };
     return(
         <div>
-            <Doughnut data={data}/>
+            <Line data={data}/>
         </div>
     )
 }
