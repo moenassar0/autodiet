2941e8cf84a22797e107400f79ee9e1646ac1389
moenassar0 <mnassar57@gmail.com>
2022-11-08 20:14:53 +0200

    frontend: updated entries page styling

--- autodiet-frontend/src/pages/user/UserWeightEntries.tsx
+++ autodiet-frontend/src/pages/user/UserWeightEntries.tsx
@@ -22,17 +22,16 @@ export const UserWeightEntries = () => {
             setDates(response.response.dates)
             setWeights(response.response.weights);
         }
-        console.log(response?.response.weightEntries);
     }
 
     return(
-        <div className="flex h-full w-full">
+        <div className="flex h-screen w-full sm:h-full flex w-full">
             <SideNavbar navbarlinks={userNavbarLinks}/>
             <div className="flex flex-col h-min-screen w-4/6 grow">
-                <AdminTopNavbar title="Meals" username="Test"> 
+                <AdminTopNavbar title="Your Weight History" username="Test"> 
                 </AdminTopNavbar>
-                <div className="flex flex-col h-min-5/6 grow w-full bg-admin-grey-background dark:bg-ad-lightgrey px-4 py-4">
-                    <div className="flex flex-wrap items-start justify-start h-1/5 w-full rounded drop-shadow bg-white dark:bg-ad-lightgrey px-2 py-2">
+                <div className="flex flex-col h-min-5/6 gap-2 grow w-full bg-admin-grey-background dark:bg-ad-lightgrey px-4 py-4">
+                    <div className="flex flex-wrap items-start justify-start h-16 w-full rounded drop-shadow bg-white dark:bg-ad-lightgrey px-2 py-2">
                         <input className="w-1/3" type="date">
 
                         </input>
@@ -41,12 +40,12 @@ export const UserWeightEntries = () => {
                         </input>
                         <button className="w-1/4 dark:bg-ad-golden dark:text-black text-white bg-admin-button rounded-full">Submit</button>
                     </div>
-                    <div className="h-5/6 grow w-full bg-white ">
+                    <div className="h-auto grow w-full bg-white ">
                         <DoughnutChart labels={dates} dataFields={weights} />
                     </div>
-                    <div className="flex w-full h-10">
+                    <div className="flex w-full h-16 bg-black mt-1 mb-1 rounded">
 
-                    </div>
+                    </div>  
                 </div>
             </div>
         </div>
