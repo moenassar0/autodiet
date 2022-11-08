0dffa204906e366e14c2225a54a17209b3e76372
moenassar0 <mnassar57@gmail.com>
2022-11-08 20:14:13 +0200

    frontend: populated chart using fetched data

--- autodiet-frontend/src/components/charts/DoughnutChart.tsx
+++ autodiet-frontend/src/components/charts/DoughnutChart.tsx
@@ -2,27 +2,13 @@ import { Doughnut, Line} from "react-chartjs-2"
 import {Chart, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title} from 'chart.js'
 Chart.register(ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title);
 
-export const DoughnutChart = () => {
-
-    
-
-    const data2 = {
-        labels: [
-            "20/11/2015", "21/11/2015", "22/11/2015"
-        ],
-        datasets: [
-            {
-                data: [83, 82, 81]
-            }
-        ]
-    }
-    const labels =  ["20/11/2015", "21/11/2015", "22/11/2015"]
+export const DoughnutChart: React.FC<{labels: Array<string>, dataFields: Array<number>}> = ({labels, dataFields}) => {
+    console.log(dataFields);
     const data = {
-
         labels: labels,
         datasets: [{
           label: 'My First Dataset',
-          data: [65, 59, 80],
+          data: dataFields,
           fill: false,
           borderColor: 'rgb(75, 192, 192)',
           tension: 0.1
