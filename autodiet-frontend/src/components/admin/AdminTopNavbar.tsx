21a8b7a92ff6fcc8a95004560db4e0b92a9faea3
moenassar0 <mnassar57@gmail.com>
2022-11-08 20:14:22 +0200

    frontend: fixed styling

--- autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -27,8 +27,8 @@ export const AdminTopNavbar:React.FC<{title: string, username: string, children:
     }, [theme])
 
     return(
-        <div className="flex gap-4 h-1/6 overflow-x-scroll max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
-            <span className="flex items-center pl-6 font-medium text-2xl dark:text-ad-golden text-admin-main">{title}</span>
+        <div className="flex gap-4 h-1/6 overflow-auto max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
+            <span className="flex h-full w-96 items-center pl-6 font-medium text-2xl dark:text-ad-golden text-admin-main">{title}</span>
             <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="ml-auto flex items-center justify-center self-center bg-admin-grey-background dark:text-black dark:bg-ad-golden drop-shadow h-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
                 <FontAwesomeIcon icon={faLightbulb} className="w-10 text-blue" />
             </div>
