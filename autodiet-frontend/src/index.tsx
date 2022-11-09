af2305e5f90c3c651bd3d6adbd8ed59b95ced069
moenassar0 <mnassar57@gmail.com>
2022-11-09 13:40:04 +0200

    frontend: wrapped app in themeprovider

--- autodiet-frontend/src/index.tsx
+++ autodiet-frontend/src/index.tsx
@@ -6,15 +6,18 @@ import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter
 } from "react-router-dom";
+import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
