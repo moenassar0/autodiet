c2dec29ad8cb6d0267fab0bee34d03f5a4a00dbb
moenassar0 <mnassar57@gmail.com>
2022-11-09 15:26:28 +0200

    frontend: piechart base fixed

---
+++ autodiet-frontend/src/components/charts/PieChart.tsx
@@ -0,0 +1,39 @@
+import { Doughnut, Line, Pie} from "react-chartjs-2"
+import {Chart, ArcElement, CategoryScale, LinearScale, PointElement, LineElement, Title} from 'chart.js'
+import { useEffect, useState } from "react";
+import { Button } from "../utility/Button";
+import type { ChartData, ChartOptions } from 'chart.js';
+import { Chart as ChartJS, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip);
+
+export const PieChart: React.FC<{labels: Array<string>, dataFields: Array<number>}> = ({labels, dataFields}) => {
+    console.log(dataFields);
+    const [color, setColor] = useState('rgb(75, 192, 192)')
+
+      const data = {
+        labels: ['Carbs', 'Fat', 'Protein'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [58, 12, 30],
+            backgroundColor: [
+              '#FDAD00',
+              '#E7CA56',
+              '#ffffff',
+            ],
+            borderColor: [
+              '#000000',
+              '#000000',
+              '#000000',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
+    return(
+        <div>
+            <Pie data={data} />
+        </div>
+    )
+}
