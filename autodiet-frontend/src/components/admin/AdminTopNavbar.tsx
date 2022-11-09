b83958d7443b283d66d3aad6f2aa7182c7023271
moenassar0 <mnassar57@gmail.com>
2022-11-09 11:18:41 +0200

    frontend: fixed topnavbar

--- autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
+++ autodiet-frontend/src/components/admin/AdminTopNavbar.tsx
@@ -27,15 +27,17 @@ export const AdminTopNavbar:React.FC<{title: string, username: string, children:
     }, [theme])
 
     return(
-        <div className="flex gap-4 h-1/6 overflow-auto max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
-            <span className="flex h-full w-96 items-center pl-6 font-medium text-2xl dark:text-ad-golden text-admin-main">{title}</span>
-            <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="ml-auto flex items-center justify-center self-center bg-admin-grey-background dark:text-black dark:bg-ad-golden drop-shadow h-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
-                <FontAwesomeIcon icon={faLightbulb} className="w-10 text-blue" />
-            </div>
-            {children}
-            <div className="flex items-center self-center w-40 h-10 rounded">
-                <img className="w-6 h-6 rounded-full mr-2" src="../logo512.png"></img>
-                <span className="w-1/3 text-md dark:text-slate-200">{username}</span>
+        <div className="flex gap-4 h-1/6 overflow-x-scroll max-h-16 w-full bg-white drop-shadow justify-start dark:bg-admin-dark-topnav">
+            <div className="h-full w-min-10 flex items-center pl-6 font-medium text-2xl dark:text-ad-golden text-admin-main">{title}</div>
+            <div className="flex h-full w-2/6 items-center ml-auto mr-2 gap-2">
+                <div onClick={() => {setTheme(theme === "light" ? "dark" : "light")}} className="ml-auto flex items-center justify-center self-center bg-admin-grey-background dark:text-black dark:bg-ad-golden drop-shadow h-9 mr-4 rounded-full hover:bg-slate-200 cursor-pointer">
+                    <FontAwesomeIcon icon={faLightbulb} className="w-10 text-blue" />
+                </div>
+                {children}
+                <div className="flex items-center self-center w-40 h-10 rounded">
+                    <img className="w-6 h-6 rounded-full" src="../logo512.png"></img>
+                    <span className="w-1/3 text-md dark:text-slate-200">{username}</span>
+                </div>
             </div>
         </div>
     )
