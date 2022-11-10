6700a15280ca93297f48b396e70e72d33f49fc24
moenassar0 <mnassar57@gmail.com>
2022-11-11 00:14:51 +0200

    frontend: nutrition details component extracted

--- autodiet-frontend/src/pages/user/Home.tsx
+++ autodiet-frontend/src/pages/user/Home.tsx
@@ -18,6 +18,7 @@ import { PieChart } from "../../components/charts/PieChart";
 import { addOrUpdateUserMeals, getUserMeals } from "../../api/services/Users";
 import { CustomNotification } from "../../components/utility/CustomNotification";
 import { drawNotification } from "../../DrawFunctions";
+import { NutritionDetails } from "../../components/utility/NutritionDetails";
 
 export const Home = () => {
     const [date, setDate] = useState(new Date());
@@ -100,28 +101,7 @@ export const Home = () => {
                             {meals?.map((meal: MealInterface) => (<div key={meal.id}><Meal meal={meal}></Meal></div>))}
                         </div>
                     </div>
-                    <div className="hidden sm:flex content-start gap-4 flex-wrap h-full grow w-3/12 bg-admin-grey-background px-2 py-2 dark:bg-[#1F1F1F] overflow-y-scroll">
-                        <div className="hidden lg:flex h-48 w-full justify-center">
-                            {meals ? <PieChart nutritionData={nutritionData} labels={[]} dataFields={[]}/> : ""}
-                        </div>
-                        <div className="flex dark:text-ad-golden w-full flex-col h-auto bg-white drop-shadow hover:drop-shadow-xl dark:bg-admin-dark-background dark:hover:opacity-80 rounded px-2 py-2 cursor-pointer">
-                            <div className="flex w-full">
-                                <span className="text-center text-xl w-full">Calories 2130</span>
-                            </div>
-                            <div className="flex w-full">
-                                <span className="text-lg w-full">Protein:</span>
-                                <span className="text-lg">{Math.round(nutritionData.protein)}</span>
-                            </div>
-                            <div className="flex w-full">
-                                <span className="text-lg w-full">Fat:</span>
-                                <span className="text-lg">{Math.round(nutritionData.fats)}</span>
-                            </div>
-                            <div className="flex w-full">
-                                <span className="text-lg w-full">Carbohydrates:</span>
-                                <span className="flex self-end">{Math.round(nutritionData.carbs)}</span>
-                            </div>
-                        </div>
-                    </div>
+                    <NutritionDetails meals={meals} nutritionData={nutritionData}></NutritionDetails>
                 </div>
             </div>
         </div>
