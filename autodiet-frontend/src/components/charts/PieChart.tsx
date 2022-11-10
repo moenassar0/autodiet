f06632730cc2c2aee2c380ba937754957c3962ac
moenassar0 <mnassar57@gmail.com>
2022-11-10 18:58:47 +0200

    frontend: multitheme implemented to piechart

--- autodiet-frontend/src/components/charts/PieChart.tsx
+++ autodiet-frontend/src/components/charts/PieChart.tsx
@@ -4,24 +4,30 @@ import { useEffect, useState } from "react";
 import { Button } from "../utility/Button";
 import type { ChartData, ChartOptions } from 'chart.js';
 import { Chart as ChartJS, Tooltip, Legend } from 'chart.js';
+import { useTheme } from "../../context/ThemeContext";
 
 ChartJS.register(ArcElement, Tooltip);
 
 export const PieChart: React.FC<{nutritionData: any, labels: Array<string>, dataFields: Array<number>}> = ({nutritionData, labels, dataFields}) => {
     console.log(nutritionData);
-    const [color, setColor] = useState('rgb(75, 192, 192)')
+    const darkColors = ['#FDAD00','#E7CA56','#ffffff'];
+    const lightColors = ['#00BAC7', '#0E2238', '#ffffff'];
+    const [colors, setColors] = useState<Array<string>>(lightColors);
 
+    const {themeType} = useTheme();
+
+    useEffect(() => {
+      if(themeType === 'dark') setColors(darkColors);
+      else setColors(lightColors);
+    }, [themeType])
+    
       const data = {
         labels: ['Carbs', 'Fat', 'Protein'],
         datasets: [
           {
             label: '# of Votes',
-            data: [nutritionData.carbs, nutritionData.protein, nutritionData.fats],
-            backgroundColor: [
-              '#FDAD00',
-              '#E7CA56',
-              '#ffffff',
-            ],
+            data: [nutritionData.carbs, nutritionData.fats, nutritionData.protein],
+            backgroundColor: colors,
             borderColor: [
               '#000000',
               '#000000',
