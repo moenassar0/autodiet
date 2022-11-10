429bc335448fab164aad523b039d35364583de71
moenassar0 <mnassar57@gmail.com>
2022-11-10 14:16:32 +0200

    frontend: custom notification base added

---
+++ autodiet-frontend/src/components/utility/CustomNotification.tsx
@@ -0,0 +1,12 @@
+export const  CustomNotification = () => {
+    return(
+        <div className="absolute bottom-0 right-0 h-min-screen h-28 w-72 rounded bg-black mr-4 mb-4">
+            <div className="flex h-1/4 w-full items-center justify-center">
+                <span className="text-lg text-ad-golden">Notification</span>
+            </div>
+            <div className="flex h-3/4 w-full text-ad-golden">
+                <span>Please go to your profile to save your details!</span>
+            </div>
+        </div>
+    )
+}
