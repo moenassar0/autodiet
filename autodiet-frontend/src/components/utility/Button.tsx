4556350e09e13a4cf03ea8d14a8e05c66aaee2af
moenassar0 <mnassar57@gmail.com>
2022-11-10 13:25:23 +0200

    frontend: button component updated for better styling

--- autodiet-frontend/src/components/utility/Button.tsx
+++ autodiet-frontend/src/components/utility/Button.tsx
@@ -1,6 +1,6 @@
-export const Button: React.FC<{title: string, onclickMethod: any}> = ({title, onclickMethod}) => {
+export const Button: React.FC<{styling:string, title: string, onclickMethod: any}> = ({title, onclickMethod, styling = ""}) => {
     return(
-        <button onClick={onclickMethod} className='flex w-28 justify-center items-center h-10 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium'>
+        <button onClick={onclickMethod} className={(styling) + ' ease-in duration-150 flex w-28 justify-center items-center h-10 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium'}>
             {title}
         </button>
     )
