60534c8e5a352a909e94d31d7b985c2ae0da64fe
moenassar0 <mnassar57@gmail.com>
2022-11-10 23:42:45 +0200

    frontend: table added

--- autodiet-frontend/src/pages/admin/AdminFoods.tsx
+++ autodiet-frontend/src/pages/admin/AdminFoods.tsx
@@ -1,6 +1,10 @@
+import { faPlus } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
+import { DataTable } from "../../components/admin/DataTable"
 import { SideNavbar } from "../../components/admin/SideNavbar"
 import { adminNavbarLinks } from "../../types/consts"
+import { FoodItemHeaders } from "../../types/types"
 
 export const AdminFoods = () => {
     return(
@@ -10,7 +14,18 @@ export const AdminFoods = () => {
                 <AdminTopNavbar title="" username="Admin">
                 </AdminTopNavbar>
                 <div className="h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F] px-4 py-4">
-                </div>
+                        <div className="flex items-center justify-start h-12 w-full rounded-t bg-white dark:bg-admin-dark-background dark:text-ad-golden px-3 pl-8">
+                            <span className="text-lg font-semibold dark:text-ad-golden">Food Items</span>
+                            <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:bg-ad-golden dark:text-black rounded-full px-1 py-1">
+                                <button>Add Food Item<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
+                            </div>
+                        </div>
+                        <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
+                            <div className="flex flex-col w-full h-auto">
+                                {foods ? <DataTable data={foods} headers={FoodItemHeaders}></DataTable> : ""}
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     )
