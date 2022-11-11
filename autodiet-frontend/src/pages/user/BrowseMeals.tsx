b64425d4c46acfafa2272b2cada06016225e1715
moenassar0 <mnassar57@gmail.com>
2022-11-11 15:06:22 +0200

    frontend: added icon

--- autodiet-frontend/src/pages/user/BrowseMeals.tsx
+++ autodiet-frontend/src/pages/user/BrowseMeals.tsx
@@ -8,6 +8,8 @@ import { EmptyState } from "../../components/utility/EmptyState";
 import { SideNavbar } from "../../components/admin/SideNavbar";
 import { userNavbarLinks } from "../../types/consts";
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 export const BrowseMeals = () => {
 
@@ -33,12 +35,15 @@ export const BrowseMeals = () => {
             <SideNavbar navbarlinks={userNavbarLinks}/>
             <div className="flex flex-col h-min-screen w-4/6 grow">
                 <AdminTopNavbar title="Meals" username="Test">
+                <div className="px-2 flex self-center h-10 w-full py-2 dark:bg-[#1D1D1E] rounded">
+                    <div className="flex w-10 h-full items-center justify-end">
+                            <FontAwesomeIcon icon={faSearch} className="text-admin-main dark:text-ad-golden" />
+                        </div>
+                        <input onChange={(e) => setSearchInput(e.target.value)} type="text" placeholder="Search.."
+                        className="w-full h-full flex px-2 py-2 outline-0 dark:bg-[#1F1F1F] dark:text-ad-golden" />
+                    </div>
                 </AdminTopNavbar>
                 <div className="flex flex-col h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F]">
-                    <div className="px-2 flex h-10 w-full py-2">
-                        <div className="flex w-10 h-full"></div>
-                        <input onChange={(e) => setSearchInput(e.target.value)} type="text" className="w-full h-full flex px-2 py-2 rounded" />
-                    </div>
                     <div className="flex flex-wrap h-auto w-full overflow-auto px-4 py-4">
                     {!(fetchedMeals.length > 0) 
                         ? <EmptyState/>
