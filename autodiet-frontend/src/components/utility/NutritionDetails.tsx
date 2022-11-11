49e3f0ba4df626d6aec303ee601b63958feb1b40
moenassar0 <mnassar57@gmail.com>
2022-11-11 13:04:40 +0200

    frontend: fixed styling

--- autodiet-frontend/src/components/utility/NutritionDetails.tsx
+++ autodiet-frontend/src/components/utility/NutritionDetails.tsx
@@ -4,7 +4,7 @@ import { PieChart } from "../charts/PieChart"
 export const NutritionDetails: React.FC<{meals: MealInterface, nutritionData: any}> = ({meals, nutritionData}) => {
     return(
         <div className="hidden sm:flex content-start gap-4 flex-wrap h-full grow w-3/12 bg-admin-grey-background px-2 py-2 dark:bg-[#1F1F1F] overflow-y-scroll">
-            <div className="hidden lg:flex h-48 w-full justify-center">
+            <div className="hidden lg:flex h-54 w-full justify-center">
                 {meals ? <PieChart nutritionData={nutritionData} labels={[]} dataFields={[]}/> : ""}
             </div>
             <div className="flex dark:text-ad-golden w-full flex-col h-auto bg-white drop-shadow hover:drop-shadow-xl dark:bg-admin-dark-background dark:hover:opacity-80 rounded px-2 py-2 cursor-pointer">
