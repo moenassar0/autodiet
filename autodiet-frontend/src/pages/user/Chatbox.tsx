335f7320bb0cbd043bd6a76b5b4acad73a792637
moenassar0 <mnassar57@gmail.com>
2022-11-12 00:57:55 +0200

    frontend: styling updated

--- autodiet-frontend/src/pages/user/Chatbox.tsx
+++ autodiet-frontend/src/pages/user/Chatbox.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import { db } from "../../api/firebase"
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
 import { SideNavbar } from "../../components/admin/SideNavbar"
+import { Button } from "../../components/utility/Button"
 import { userNavbarLinks } from "../../types/consts"
 
 export const Chatbox = () => {
@@ -27,16 +28,17 @@ export const Chatbox = () => {
             <div className="flex flex-col h-min-screen w-4/6 grow">
                 <AdminTopNavbar title="Meals" username="Test">
                 </AdminTopNavbar>
-                <div className="flex flex-col h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F] px-2 py-2 dark:text-ad-golden">
-                    <div className="flex flex-col h-5/6 w-full px-3 py-2 rounded bg-[#2D2D2D] gap-1">
+                <div className="flex flex-col h-9/12 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F] px-2 py-2 dark:text-ad-golden">
+                    <div className="flex flex-col h-5/6 w-full px-3 py-2 rounded bg-white drop-shadow dark:bg-admin-dark-background gap-1">
                     {messages?.map((message: any, i: number) => (
                             <div key={i} className="flex w-full h-10">
-                                <span className="rounded bg-[#1E1E1E] p-1">{message.text}</span>
+                                <span className="rounded dark:bg-[#1E1E1E] bg-admin-grey-background p-1">{message.text}</span>
                             </div>
                         ))}
                     </div>
-                    <div className="bg-white flex w-full h-1/5 rounded">
-                        <input className=""></input>
+                    <div className="flex items-center justify-between border-t p-1 border-gray-300 bg-white rounded-b dark:bg-[#2D2D2D] flex w-full h-3/12">
+                        <input placeholder="Type a message" className="flex w-2/3 p-1 bg-admin-grey-background dark:bg-admin-dark-background text-gray-300"></input>
+                        <Button title="Send" onclickMethod={() => { } } styling={"w-1/3"}></Button>
                     </div>
                 </div>
             </div>
