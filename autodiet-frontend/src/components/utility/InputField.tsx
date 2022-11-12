86e7b2c5965d8d7b3955f3dc76f3c53c68346c15
moenassar0 <mnassar57@gmail.com>
2022-11-12 18:38:28 +0200

    frontend: setting value to prop value

--- autodiet-frontend/src/components/utility/InputField.tsx
+++ autodiet-frontend/src/components/utility/InputField.tsx
@@ -8,7 +8,8 @@ const InputField: React.FC<InputFieldInterface> = ({title, error, state, setHook
         <>
             <div className="flex items-center place-content-between w-full h-10 px-2 py-1">
                 <span className="w-2/5 text-admin-main dark:text-ad-golden text-lg">{title}</span>
-                <input 
+                <input
+                    value={state} 
                     onChange={(e) => setHook(e.target.value)} 
                     className={"h-full w-3/5 py-2 px-4 bg-admin-main text-white dark:bg-black rounded focus:outline-none focus:ring-2 focus:border-admin-button focus:ring-admin-button dark:focus:border-ad-golden dark:focus:ring-ad-golden" +
                     (!valid && state ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="text">
