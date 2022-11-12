33409f099addcd9dd3f71dae627a30b53c42b811
moenassar0 <mnassar57@gmail.com>
2022-11-12 17:53:18 +0200

    frontend: fixed link

--- autodiet-frontend/src/types/consts.ts
+++ autodiet-frontend/src/types/consts.ts
@@ -14,7 +14,7 @@ export const userNavbarLinks = [
 export const adminNavbarLinks = [
     { path: "/admin/users", icon: faUser, title: "Users" },
     { path: "/admin/meals", icon: faPlateWheat, title: "Meals" },
-    { path: "/admin/food", icon: faDrumstickBite, title: "Foods" },
+    { path: "/admin/foods", icon: faDrumstickBite, title: "Foods" },
     { path: "/admin/users", icon: faChartLine, title: "Graphs" },
     { path: "/admin/users", icon: faRightFromBracket, title: "Logout" },
 ];
