17bda30da59f4a4ca63678faa16acce4763a38e8
moenassar0 <mnassar57@gmail.com>
2022-11-12 13:34:38 +0200

    frontend: added close button

--- autodiet-frontend/src/components/utility/Popup.tsx
+++ autodiet-frontend/src/components/utility/Popup.tsx
@@ -1,12 +1,15 @@
 import { InputFieldInterface } from "../../types/types";
 import InputField from "./InputField";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClose } from '@fortawesome/free-solid-svg-icons'
 
-export const Popup: React.FC<{message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({title, inputs, submitMethod, message}) => {
+export const Popup: React.FC<{children: any, message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({children, title, inputs, submitMethod, message}) => {
     return(
         <div className="absolute flex w-full h-screen z-20 items-center justify-center">
-        <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-y-scroll gap-2 rounded-t-lg">
+        <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
             <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
                 <span>{title + "   "} {message ? message : ""}</span>
+                <span className="ml-auto">{children}</span>
             </div>
             {
                 inputs.map((input, i) => (
