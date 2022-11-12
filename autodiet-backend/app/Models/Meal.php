a9d7d8d987f32f79d428b39ce54c5ed36693c53c
moenassar0 <mnassar57@gmail.com>
2022-11-12 19:06:17 +0200

    backend: added fillable to meal model

--- autodiet-backend/app/Models/Meal.php
+++ autodiet-backend/app/Models/Meal.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meal extends Model
 {
+    protected $fillable = [
+        'title',
+        'calories',
+        'protein',
+        'carbohydrate',
+        'fat',
+        'type',
+        'protein_percentage',
+        'picture_url',
+    ];
+
     public function recipe()
     {
         return $this->belongsToMany(FoodItem::class, 'meal_recipes','meal_id','recipe_item_id')
