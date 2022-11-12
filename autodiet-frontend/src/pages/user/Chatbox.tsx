9bf8c115ee75dc1ac84e3c64257ae25aa89890c7
moenassar0 <mnassar57@gmail.com>
2022-11-13 01:10:11 +0200

    frontend: fetching messages fixed

--- autodiet-frontend/src/pages/user/Chatbox.tsx
+++ autodiet-frontend/src/pages/user/Chatbox.tsx
@@ -1,4 +1,4 @@
-import { query, collection, onSnapshot, orderBy, addDoc, serverTimestamp } from "firebase/firestore"
+import { query, collection, onSnapshot, orderBy, addDoc, serverTimestamp, where } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { db } from "../../api/firebase"
 import { getUser } from "../../api/services/Users"
@@ -6,6 +6,7 @@ import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
 import { SideNavbar } from "../../components/admin/SideNavbar"
 import { Button } from "../../components/utility/Button"
 import { userNavbarLinks } from "../../types/consts"
+import { UserInterface } from "../../types/types"
 
 export const Chatbox = () => {
 
@@ -13,17 +14,32 @@ export const Chatbox = () => {
     const [inputMessage, setInputMessage] = useState("");
 
     useEffect(() => {
-        const q = query(collection(db, 'messages'), orderBy('timestamps'));
-        const unsubscribe = onSnapshot(q, (querySnapshot: any) => {
-            let messages: any = [];
-            querySnapshot.forEach((doc: any) => {
-                messages.push({...doc.data(), id: doc.id});
-            });
-            console.log(messages);
-            setMessages(messages);
-        })
-        return () => unsubscribe()
+        let userID: any = -1;
+        getUserID().then((id) => {
+            userID = id
+            console.log(userID);
+            const q = query(collection(db, 'messages'), where("userID", "==", userID),orderBy('timestamps'));
+            const unsubscribe = onSnapshot(q, (querySnapshot: any) => {
+                let messages: any = [];
+                querySnapshot.forEach((doc: any) => {
+                    messages.push({...doc.data(), id: doc.id});
+                });
+                console.log(messages);
+                setMessages(messages);
+            })
+            return () => unsubscribe()
+        });
     }, [])
+
+    const getUserID = async () => {
+        const response = await getUser();
+        if(response?.response){
+            const userID = response.response.id;
+            const userName = response.response.username;
+            let user: UserInterface = {username: userName, id: userID, email:""};
+            return user.id;
+        }
+    }
 
     const sendMessage = async () => {
         const response = await getUser();
@@ -55,7 +71,7 @@ export const Chatbox = () => {
                         ))}
                     </div>
                     <div className="flex items-center justify-between border-t p-1 border-gray-300 bg-white rounded-b dark:bg-[#2D2D2D] flex w-full h-3/12">
-                        <input type="text" value={inputMessage} onChange={(e) => {setInputMessage(e.currentTarget.value)}} className="outline-none flex w-2/3 p-1 bg-admin-grey-background dark:bg-admin-dark-background text-gray-300"></input>
+                        <input type="text" placeholder="Type a message" value={inputMessage} onChange={(e) => {setInputMessage(e.currentTarget.value)}} className="outline-none flex w-2/3 p-1 bg-admin-grey-background dark:bg-admin-dark-background dark:text-ad-golden text-black"></input>
                         <Button title="Send" onclickMethod={sendMessage} styling={"w-1/3"}></Button>
                     </div>
                 </div>
