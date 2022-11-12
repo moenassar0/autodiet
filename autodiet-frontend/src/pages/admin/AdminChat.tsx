fd714cb11dcc4d89b639310fc50e789485c9fc68
moenassar0 <mnassar57@gmail.com>
2022-11-12 22:45:36 +0200

    frontend: chat base added

---
+++ autodiet-frontend/src/pages/admin/AdminChat.tsx
@@ -0,0 +1,11 @@
+import { AdminBase } from "../../layouts/AdminBase"
+
+export const AdminChat = () => {
+    return(
+        <AdminBase navbarTitle="Chat Requests" navbarProps={<></>} >
+            <div className="flex w-full h-full bg-black">
+                
+            </div>
+        </AdminBase>
+    )
+}
