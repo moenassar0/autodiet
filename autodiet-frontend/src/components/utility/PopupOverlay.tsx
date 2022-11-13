5d2625733fe9d10d5285a6c56155b948adb7870c
moenassar0 <mnassar57@gmail.com>
2022-11-13 22:08:44 +0200

    frontend: anim duration added

--- autodiet-frontend/src/components/utility/PopupOverlay.tsx
+++ autodiet-frontend/src/components/utility/PopupOverlay.tsx
@@ -1,7 +1,7 @@
 export const PopupOverlay = () => {
 
     return(
-        <div className="flex absolute top-0 left-0 w-full h-screen bg-black opacity-50 z-10">
+        <div className="ease-in duration-600 flex absolute top-0 left-0 w-full h-screen bg-black opacity-50 z-10">
         </div>
     )
 
