bbb924d2f859b72986cb5056c7ffa9423459f379
moenassar0 <mnassar57@gmail.com>
2022-11-14 16:00:38 +0200

    frontend: code cleanup and dividing into components

--- autodiet-frontend/src/pages/user/Profile.tsx
+++ autodiet-frontend/src/pages/user/Profile.tsx
@@ -6,6 +6,7 @@ import axios from "../../api/axios";
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar";
 import { userNavbarLinks } from "../../types/consts";
 import { SideNavbar } from "../../components/admin/SideNavbar";
+import { ProfileField } from "../../components/utility/ProfileField";
 
 export const Profile = () => {
 
@@ -18,6 +19,10 @@ export const Profile = () => {
     const [age, setAge] = useState('');
     const [height, setHeight] = useState('');
     const [bodyFatPercentage, setBodyFatPercentage] = useState('');
+    const [activity, setActivity] = useState('');
+
+    const goalButtonValues = ["Lose Weight", "Maintain", "Gain Muscle"];
+    const bfButtonValues = ["Lean", "Medium", "High"];
 
     useEffect(() => {
         fetchUserData();
@@ -61,46 +66,42 @@ export const Profile = () => {
                 ? <div className="flex flex-col items-center justify-center h-full w-full"><img src="../logo2.png" className="h-14 w-28"></img><img src="../gh.gif" className="h-14 w-14"></img></div> 
                 : 
                 <div className="flex h-4/5 grow w-full bg-admin-grey-background dark:bg-ad-lightgrey">
-                    <div className="flex flex-col h-4/5 w-5/6 py-2 px-2">
+                    <div className="w-full flex flex-col h-4/5 sm:w-5/6 py-2 px-2">
+                        <ProfileField title="Goal" buttonValues={goalButtonValues} hook={activeGoal} setHook={setActiveGoal}>
+
+                        </ProfileField>
                         <div className="flex w-full h-1/6">
-                            <div className="flex items-center w-1/5 h-full font-medium">Goal</div>
-                            <div className="flex w-4/5 sm:flex w-3/5 h-full items-center justify-end">
-                                <button onClick={() => {setActiveGoal("LoseWeight")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (activeGoal==="LoseWeight" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Lose weight</button>
-                                <button onClick={() => {setActiveGoal("Maintain")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (activeGoal==="Maintain" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Maintain</button>
-                                <button onClick={() => {setActiveGoal("GainMuscle")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-r-lg " + (activeGoal==="GainMuscle" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Gain muscle</button>
-                            </div>
-                        </div>
-                        <div className="flex w-full h-1/6">
-                            <div className="flex items-center  w-1/5 h-full font-medium">Sex</div>
+                            <div className="flex items-center  w-1/5 h-full font-medium dark:text-ad-golden text-black">Sex</div>
                             <div className="flex w-3/5 h-full items-center justify-end">
-                                <button onClick={() => {setSex("Male")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-l-lg " + (sex ==="Male" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Male</button>
-                                <button onClick={() => {setSex("Female")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-r-lg " + (sex === "Female" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Female</button>
+                                <button onClick={() => {setSex("Male")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-l-lg " + (sex ==="Male" ? "dark:bg-ad-golden bg-admin-button" : "bg-white hover:bg-slate-200")}>Male</button>
+                                <button onClick={() => {setSex("Female")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/2 rounded-r-lg " + (sex === "Female" ? "dark:bg-ad-golden bg-admin-button" : "bg-white hover:bg-slate-200")}>Female</button>
                             </div>
                         </div>
                         <div className="flex items-center w-full h-1/6">
-                            <div className="flex items-center w-1/5 h-full font-medium">Height</div>
+                            <div className="flex items-center w-1/5 h-full font-medium dark:text-ad-golden text-black">Height</div>
                             <div className="flex w-4/5 sm:flex w-3/5 h-1/2 justify-start">
                                 <input type="text" value={height} onChange={(e) => setHeight(e.target.value)} className="w-1/2 h-full rounded px-1 py-1"></input>
                             </div>
                         </div>
                         <div className="flex items-center w-full h-1/6">
-                            <div className="flex items-center w-1/5 h-full font-medium">Age</div>
+                            <div className="flex items-center w-1/5 h-full font-medium dark:text-ad-golden text-black">Age</div>
                             <div className="flex w-4/5 sm:flex w-3/5 h-1/2 justify-start">
                                 <input type="text" value={age} onChange={(e) => setAge(e.target.value)} className="w-1/2 h-full rounded px-1 py-1"></input>
                             </div>
                         </div>
                         <div className="flex items-center w-full h-1/6">
-                            <div className="flex items-center w-1/5 h-full font-medium">Weight</div>
+                            <div className="flex items-center w-1/5 h-full font-medium dark:text-ad-golden text-black">Weight</div>
                             <div className="flex w-4/5 sm:flex w-3/5 h-1/2 justify-start">
                                 <input type="text" value={weight} onChange={(e) => setWeight(e.target.value)} className="w-1/2 h-full rounded px-1 py-1"></input>
                             </div>
                         </div>
+                        <ProfileField title="Bodyfat Percentage" hook={bodyFatPercentage} setHook={setBodyFatPercentage} buttonValues={bfButtonValues}>
+                            
+                        </ProfileField>
                         <div className="flex w-full h-1/6">
-                            <div className="flex items-center w-1/5 h-full font-medium">Body Percentage</div>
+                            <div className="flex items-center w-2/5 h-full text-white"></div>
                             <div className="flex w-4/5 sm:flex w-3/5 h-full items-center justify-end">
-                                <button onClick={() => {setBodyFatPercentage("Lean")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (bodyFatPercentage==="Lean" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Lean</button>
-                                <button onClick={() => {setBodyFatPercentage("Medium")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 " + (bodyFatPercentage==="Medium" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>Medium</button>
-                                <button onClick={() => {setBodyFatPercentage("High")}} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-r-lg " + (bodyFatPercentage==="High" ? "dark:bg-ad-golden bg-admin-button" : "bg-white")}>High</button>
+                                <button onClick={() => updateUserDetails()} className={"flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded dark:bg-ad-golden bg-admin-button"}>Save Changes</button>
                             </div>
                         </div>
                         <div className="flex w-full h-1/6">
