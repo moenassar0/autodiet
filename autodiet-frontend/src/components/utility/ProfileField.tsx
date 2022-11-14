d0b1ff611acc9181c181bbb218f44ce1473eeae4
moenassar0 <mnassar57@gmail.com>
2022-11-14 16:47:12 +0200

    frontend: styling updated

--- autodiet-frontend/src/components/utility/ProfileField.tsx
+++ autodiet-frontend/src/components/utility/ProfileField.tsx
@@ -1,13 +1,13 @@
 export const ProfileField: React.FC<{buttonValues:any, children: any, title: string, hook: string, setHook: any}> = ({children, title, hook, setHook, buttonValues}) => {
     
     return(
-        <div className="flex w-full h-1/6 drop-shadow-md">
+        <div className="flex w-full h-1/6">
             <div className="flex items-center w-1/5 h-full font-medium dark:text-ad-golden text-black">{title}</div>
-            <div className="flex w-4/5 sm:flex w-3/5 h-full items-center justify-end">
+            <div className="flex w-4/5 sm:flex w-3/5 h-full items-center justify-end drop-shadow-md">
                 {
                     buttonValues.map((button: string) => (
                         <button onClick={() => {setHook(button)}}
-                        className={"text-xs flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded-l-lg " + (hook===button ? "dark:bg-ad-golden bg-admin-button" : "bg-white hover:bg-slate-200")}>
+                        className={"text-xs sm:text-lg flex items-center justify-center h-1/2 justify-self-end w-1/3 rounded " + (hook===button ? "dark:bg-ad-golden bg-admin-button" : "bg-white hover:bg-slate-200")}>
                             {button}
                         </button>
                     ))
