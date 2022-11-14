7de10ffc6be9765306fc0c5efa796e6412bd12ba
moenassar0 <mnassar57@gmail.com>
2022-11-15 01:31:20 +0200

    frontend: inputs added

---
+++ autodiet-frontend/src/components/admin/FoodPopup.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { InputFieldInterface } from "../../types/types";
+
+export const FoodPopup: React.FC<{edit:boolean, userID?: number, setTrigger: any, trigger: boolean}> = ({userID, edit, setTrigger, trigger}) => {
+    
+    const [title, setTitle] = useState("");
+    const [calories, setCalories] = useState(0);
+    const [protein, setProtein] = useState(0);
+    const [carbohydrate, setCarbohydrate] = useState(0);
+    const [fat, setFat] = useState(0);
+    const [servingSize, setServingSize] = useState('')
+    const [servingType, setServingType] = useState('');
+    const [validType, setValidType] = useState(true);
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        if(servingType == "Grams" || servingType == "Large" || servingType == "Medium") 
+            setValidType(true);
+        else setValidType(false)
+    }, [servingType])
+
+    const inputs: Array<InputFieldInterface> = [
+        { title:"Title", error:"asd", setHook:setTitle, state:title, valid:true },
+        { title:"Calories", error:"asd", setHook:setCalories, state:calories.toString(), valid:true },
+        { title:"Protein", error:"asd", setHook:setProtein, state:protein.toString(), valid:true },
+        { title:"Carbohydrate", error:"asd", setHook:setCarbohydrate, state:carbohydrate.toString(), valid:true },
+        { title:"Fat", error:"asd", setHook: setFat, state:fat.toString(), valid:true },
+        { title: "Serving Type", error:"Please choose from: Grams, Large, Medium", setHook:setServingType, state:servingType, valid:validType},
+        { title: "Serving Size", error:"", setHook:setServingSize, state:servingSize, valid:true},
+    ];
+    
+    return(
+        <>
+
+        </>
+    )
+}
