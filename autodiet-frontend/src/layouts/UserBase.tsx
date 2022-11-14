32cd01b95c24d5030ff9d987d324711e32887bd8
moenassar0 <mnassar57@gmail.com>
2022-11-15 00:44:55 +0200

    frontend: added navbar children props

--- autodiet-frontend/src/layouts/UserBase.tsx
+++ autodiet-frontend/src/layouts/UserBase.tsx
@@ -4,12 +4,13 @@ import { AdminTopNavbar } from "../components/admin/AdminTopNavbar"
 import { SideNavbar } from "../components/admin/SideNavbar"
 import { userNavbarLinks } from "../types/consts"
 
-export const UserBase: React.FC<{children: any, pageTitle: string}> = ({children, pageTitle}) => {
+export const UserBase: React.FC<{topNavbarChildren: any, children: any, pageTitle: string}> = ({topNavbarChildren, children, pageTitle}) => {
     return(
         <div className="flex h-screen w-full">
             <SideNavbar navbarlinks={userNavbarLinks}/>
             <div className="flex flex-col h-min-screen w-4/6 grow">
                 <AdminTopNavbar title={pageTitle} username="Test">
+                {topNavbarChildren}
                 </AdminTopNavbar>
                 <div className="flex flex-col h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F]">
                     {children}
