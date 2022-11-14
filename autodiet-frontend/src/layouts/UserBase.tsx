11259dddf1575008252bbb33f4f03d6511f85ef8
moenassar0 <mnassar57@gmail.com>
2022-11-14 23:32:38 +0200

    frontend: user layout added

---
+++ autodiet-frontend/src/layouts/UserBase.tsx
@@ -0,0 +1,20 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { AdminTopNavbar } from "../components/admin/AdminTopNavbar"
+import { SideNavbar } from "../components/admin/SideNavbar"
+import { userNavbarLinks } from "../types/consts"
+
+export const UserBase: React.FC<{children: any, pageTitle: string}> = ({children, pageTitle}) => {
+    return(
+        <div className="flex h-screen w-full">
+            <SideNavbar navbarlinks={userNavbarLinks}/>
+            <div className="flex flex-col h-min-screen w-4/6 grow">
+                <AdminTopNavbar title={pageTitle} username="Test">
+                </AdminTopNavbar>
+                <div className="flex flex-col h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F]">
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
+}
