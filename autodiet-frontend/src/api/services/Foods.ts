f2b6456b6344306cd5354dae9a1d269c7880e107
moenassar0 <mnassar57@gmail.com>
2022-11-15 01:55:55 +0200

    frontend: add food api function added

--- autodiet-frontend/src/api/services/Foods.ts
+++ autodiet-frontend/src/api/services/Foods.ts
@@ -1,4 +1,4 @@
-import { get } from "./baseAPICall";
+import { get, post } from "./baseAPICall";
 
 export const getFoods = async () => {
     return get("/foods", "");
@@ -6,4 +6,8 @@ export const getFoods = async () => {
 
 export const getFoodsByTitle = async (search_string: string) => {
     return get("/foods/", search_string)
+}
+
+export const addFood = async (data: object) => {
+    return post("/food_item", data);
 }
