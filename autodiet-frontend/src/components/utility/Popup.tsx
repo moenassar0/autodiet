c8adf9a4a84478423afb71de6e1343f8e6c49f22
moenassar0 <mnassar57@gmail.com>
2022-11-15 01:06:10 +0200

    frontend: added additional props

--- autodiet-frontend/src/components/utility/Popup.tsx
+++ autodiet-frontend/src/components/utility/Popup.tsx
@@ -3,7 +3,7 @@ import InputField from "./InputField";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
 
-export const Popup: React.FC<{children: any, message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({children, title, inputs, submitMethod, message}) => {
+export const Popup: React.FC<{children: any, edit: boolean, message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({children, edit, title, inputs, submitMethod, message}) => {
     return(
         <div className="absolute flex w-full h-screen z-20 items-center justify-center">
         <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
@@ -20,7 +20,9 @@ export const Popup: React.FC<{children: any, message: string, submitMethod: any,
                 ))
             }
             <div className="flex w-full h-full justify-end items-center py-2 px-2">
-                <button onClick={submitMethod} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">Add</button>
+                <button onClick={submitMethod} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">
+                    {(edit ? "Edit" : "Add")}
+                </button>
             </div>
         </div>
     </div>
