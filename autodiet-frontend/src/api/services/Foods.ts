f61d7617aabcdd20c3b584d7638eb20881aa0117
moenassar0 <mnassar57@gmail.com>
2022-11-15 00:53:36 +0200

    frontend: added get foods by title function

--- autodiet-frontend/src/api/services/Foods.ts
+++ autodiet-frontend/src/api/services/Foods.ts
@@ -3,3 +3,7 @@ import { get } from "./baseAPICall";
 export const getFoods = async () => {
     return get("/foods", "");
 }
+
+export const getFoodsByTitle = async (search_string: string) => {
+    return get("/foods/", search_string)
+}
