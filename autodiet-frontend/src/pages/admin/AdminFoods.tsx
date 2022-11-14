a611e9a3633ad3a7554b866df0670d7696acebb7
moenassar0 <mnassar57@gmail.com>
2022-11-15 01:35:43 +0200

    frontend: added popup to food page

--- autodiet-frontend/src/pages/admin/AdminFoods.tsx
+++ autodiet-frontend/src/pages/admin/AdminFoods.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react"
 import { getFoods } from "../../api/services/Foods"
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
 import { DataTable } from "../../components/admin/DataTable"
+import { FoodPopup } from "../../components/admin/FoodPopup"
 import { SideNavbar } from "../../components/admin/SideNavbar"
 import { adminNavbarLinks } from "../../types/consts"
 import { FoodItemHeaders } from "../../types/types"
@@ -11,6 +12,7 @@ import { FoodItemHeaders } from "../../types/types"
 export const AdminFoods = () => {
 
     const [foods, setFoods] = useState([]);
+    const [addPopup, setAddPopup] = useState(false);
 
     async function fetchFoodItems(){
         const response = await getFoods();
@@ -23,6 +25,7 @@ export const AdminFoods = () => {
 
     return(
         <div className="flex h-screen w-full">
+            <FoodPopup edit={false} trigger={addPopup} setTrigger={setAddPopup} ></FoodPopup>
             <SideNavbar navbarlinks={adminNavbarLinks}/>
             <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
                 <AdminTopNavbar title="" username="Admin">
@@ -31,7 +34,7 @@ export const AdminFoods = () => {
                     <div className="flex items-center justify-start h-12 w-full rounded-t bg-white dark:bg-admin-dark-background dark:text-ad-golden px-3 pl-8">
                         <span className="text-lg font-semibold dark:text-ad-golden">Food Items</span>
                         <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:bg-ad-golden dark:text-black rounded-full px-1 py-1">
-                            <button>Add Food Item<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
+                            <button onClick={()=>{setAddPopup(true)}}>Add Food Item<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
                         </div>
                     </div>
                     <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
