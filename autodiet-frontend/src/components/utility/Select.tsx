f628487be11521ac788ce1b607bb8a032a987060
moenassar0 <mnassar57@gmail.com>
2022-11-14 16:47:22 +0200

    frontend: styling updated

--- autodiet-frontend/src/components/utility/Select.tsx
+++ autodiet-frontend/src/components/utility/Select.tsx
@@ -1,6 +1,6 @@
 export const Select: React.FC<{setHook: any, name: string, options: Array<string>}> = ({setHook, name, options}) => {
     return(
-        <select name="activity" onChange={(e) => {setHook(e.target.value)}}>
+        <select className="w-1/2 rounded h-2/3 drop-shadow bg-white  flex p-2" name="activity" onChange={(e) => {setHook(e.target.value)}}>
             {options.map((option) => (
                 <option value={option}>{option}</option>
             ))}
