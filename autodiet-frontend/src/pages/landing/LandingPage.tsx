bd38a3f18984ee69dab9d3445d032920eb8c2710
moenassar0 <mnassar57@gmail.com>
2022-11-15 15:29:45 +0200

    frontend: fixed container

--- autodiet-frontend/src/pages/landing/LandingPage.tsx
+++ autodiet-frontend/src/pages/landing/LandingPage.tsx
@@ -9,16 +9,16 @@ export const LandingPage = () => {
       ]
 
     return(
-      <>
-        <div className='translucent-overlay'></div>
-        <Navbar />
-        <div className='navbar-line'><hr></hr></div>
-        <div className='hero-content'>
-          <span className='hero-title'>AUTOMATE YOUR DIET</span>
-          <span className='hero-subtitle'>You don't have to waste your time.<br></br>We'll do the work for you.</span>
-          <span className='hero-description'>With a click of a button, generate the next<br />week’s entire meal plan and grocesseies list:</span>
-          <img className='hero-image' src="../../hero.png"></img>
+        <div className="relative min-h-screen h-auto w-full">
+          <div className='translucent-overlay'></div>
+          <Navbar />
+          <div className='navbar-line'><hr></hr></div>
+          <div className='hero-content'>
+            <span className='hero-title'>AUTOMATE YOUR DIET</span>
+            <span className='hero-subtitle'>You don't have to waste your time.<br></br>We'll do the work for you.</span>
+            <span className='hero-description'>With a click of a button, generate the next<br />week’s entire meal plan and grocesseies list:</span>
+            <img className='hero-image' src="../../hero.png"></img>
+          </div>   
         </div>
-      </>
     )
 }
