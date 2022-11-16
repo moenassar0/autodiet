b9535cb9c1d67517edfc800936563eb33c3480c6
moenassar0 <mnassar57@gmail.com>
2022-11-16 17:24:01 +0200

    frontend: updated macros

--- autodiet-frontend/src/components/user/Meal.tsx
+++ autodiet-frontend/src/components/user/Meal.tsx
@@ -9,11 +9,11 @@ type Props = {
 const Meal: React.FC<Props> = ({meal}) => {
 
     const [popup, setPopup] = useState(false);
-    const [mealNutrition, setMealNutrition] = useState({} as NutritionObjectInterface);
+    const [macros, setMacros] = useState("");
 
     useEffect(() => {
         const mealNutrition: NutritionObjectInterface = getNutritionFromMeal(meal);
-        setMealNutrition(mealNutrition);
+        setMacros(macrosFromNutrition(mealNutrition));
     }, [])
 
     return(
@@ -24,7 +24,7 @@ const Meal: React.FC<Props> = ({meal}) => {
                 <div className="flex flex-col justify-start dark:text-ad-golden w-full h-2/5 gap-1">
                     <span className="w-full h-1/3 font-medium ">{meal.title}</span>
                     <span className="w-full h-1/3 text-small dark:text-admin-grey-background text-slate-500">{Math.round(meal.calories)} Calories</span>
-                    <span className="w-full h-1/3 text-xs dark:text-admin-grey-background text-slate-500">{macrosFromNutrition(mealNutrition)}</span>
+                    <span className="w-full h-1/3 text-xs dark:text-admin-grey-background text-slate-500">{macros ? macros : ""}</span>
                 </div>
                 <div className="flex w-full justify-start items-center h-3/5 dark:text-white">
                     <img className="w-full h-full rounded" src={meal.picture_url}></img>
