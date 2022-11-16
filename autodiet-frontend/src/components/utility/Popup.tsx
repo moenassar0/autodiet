a7b3e807c3d5dfc6e595124d878dc72557152dd3
moenassar0 <mnassar57@gmail.com>
2022-11-16 02:13:59 +0200

    frontend: fixed popup styling for overflow

--- autodiet-frontend/src/components/utility/Popup.tsx
+++ autodiet-frontend/src/components/utility/Popup.tsx
@@ -6,7 +6,7 @@ import { faClose } from '@fortawesome/free-solid-svg-icons'
 export const Popup: React.FC<{children: any, edit: boolean, message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({children, edit, title, inputs, submitMethod, message}) => {
     return(
         <div className="absolute flex w-full h-screen z-20 items-center justify-center">
-        <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
+        <div className="w-4/5 sm:w-3/6 h-full flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
             <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
                 <span>{title + "   "} {message ? message : ""}</span>
                 <span className="ml-auto">{children}</span>
@@ -19,7 +19,7 @@ export const Popup: React.FC<{children: any, edit: boolean, message: string, sub
                     valid={input.valid}></InputField>
                 ))
             }
-            <div className="flex w-full h-full justify-end items-center py-2 px-2">
+            <div className="flex w-full h-16 justify-end items-center py-2 px-2">
                 <button onClick={submitMethod} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">
                     {(edit ? "Edit" : "Add")}
                 </button>
