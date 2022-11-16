cb50aa04e1900512329fbe4ac44880ceb76d6a36
moenassar0 <mnassar57@gmail.com>
2022-11-16 19:22:25 +0200

    frontend: added delete functionality

--- autodiet-frontend/src/pages/admin/AdminMeals.tsx
+++ autodiet-frontend/src/pages/admin/AdminMeals.tsx
@@ -4,35 +4,35 @@ import { useEffect, useState } from "react"
 import { SideNavbar } from "../../components/admin/SideNavbar"
 import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
 import { DataTable } from "../../components/admin/DataTable"
-import { getMeals } from "../../api/services/Meals";
+import { deleteMeal, getMeals } from "../../api/services/Meals";
 import { MealHeaders } from "../../types/types"
 import { adminNavbarLinks } from "../../types/consts"
 import { AddMealPopup } from "../../components/admin/AddMealPopup"
+import { PopupOverlay } from "../../components/utility/PopupOverlay"
+import { DeletePopup } from "../../components/utility/DeletePopup"
 
 export const AdminMeals = () => {
 
     const [meals, setMeals] = useState([]);
     const [addMealsPopup, setAddMealsPopup] = useState(false);
     const [editMealsPopup, setEditMealsPopup] = useState(false);
+    const [deleteMealsPopup, setDeleteMealsPopup] = useState(false);
     const [mealID, setMealID] = useState(0);
 
     async function fetchMeals(){
         const response = await getMeals();
-        if(response?.success){ setMeals(response.response.meals);
-        console.log(response)}
+        if(response?.success) setMeals(response.response.meals);
     }
 
     const editFun = (id: number) => {
         setMealID(id);
         setEditMealsPopup(true);
-        console.log(id);
-        
     }
 
     const delFunction = (id: number) => {
-        //setEditMealsPopup(true);
-        console.log(id);
-        //setMealID(id);
+        deleteMeal(mealID.toString());
+        setDeleteMealsPopup(false);
+        fetchMeals();
     }
 
     useEffect(() => {
@@ -57,11 +57,17 @@ export const AdminMeals = () => {
                         </div>
                         <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
                             <div className="flex flex-col w-full h-auto">
-                                {meals ? <DataTable editFunction={editFun} deleteFunction={delFunction} data={meals} headers={MealHeaders}></DataTable> : ""}
+                                {meals ? <DataTable editFunction={editFun} deleteFunction={(id: number) => {setMealID(id); setDeleteMealsPopup(true)}} data={meals} headers={MealHeaders}></DataTable> : ""}
                             </div>
                         </div>
                     </div>
                 </div>
+                {deleteMealsPopup ? 
+                <>
+                    <PopupOverlay></PopupOverlay>
+                    <DeletePopup trigger={deleteMealsPopup} setTrigger={setDeleteMealsPopup} submitMethod={delFunction}>
+                    </DeletePopup>
+                </> : ""}
             </div>
     )
 }
