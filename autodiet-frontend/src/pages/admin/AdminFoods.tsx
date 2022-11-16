234452724550b298c906e3aad24bf726cfb39f8f
moenassar0 <mnassar57@gmail.com>
2022-11-16 19:46:08 +0200

    frontend: layout added

--- autodiet-frontend/src/pages/admin/AdminFoods.tsx
+++ autodiet-frontend/src/pages/admin/AdminFoods.tsx
@@ -2,13 +2,11 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useEffect, useState } from "react"
 import { deleteFood, getFoods } from "../../api/services/Foods"
-import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
 import { DataTable } from "../../components/admin/DataTable"
 import { FoodPopup } from "../../components/admin/FoodPopup"
-import { SideNavbar } from "../../components/admin/SideNavbar"
 import { DeletePopup } from "../../components/utility/DeletePopup"
 import { PopupOverlay } from "../../components/utility/PopupOverlay"
-import { adminNavbarLinks } from "../../types/consts"
+import { AdminBase } from "../../layouts/AdminBase"
 import { FoodItemHeaders } from "../../types/types"
 
 export const AdminFoods = () => {
@@ -40,33 +38,28 @@ export const AdminFoods = () => {
     }, [])
 
     return(
-        <div className="flex h-screen w-full">
+        <section>
             <FoodPopup edit={false} trigger={addPopup} setTrigger={setAddPopup} ></FoodPopup>
             <FoodPopup userID={currFoodID} edit={true} trigger={editPopup} setTrigger={setEditPopup} ></FoodPopup>
-            <SideNavbar navbarlinks={adminNavbarLinks}/>
-            <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
-                <AdminTopNavbar title="" username="Admin">
-                </AdminTopNavbar>
-                <div className="h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F] px-4 py-4">
-                    <div className="flex items-center justify-start h-12 w-full rounded-t bg-white dark:bg-admin-dark-background dark:text-ad-golden px-3 pl-8">
-                        <span className="text-lg font-semibold dark:text-ad-golden">Food Items</span>
-                        <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:bg-ad-golden dark:text-black rounded-full px-1 py-1">
-                            <button onClick={()=>{setAddPopup(true)}}>Add Food Item<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
-                        </div>
-                    </div>
-                    <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
-                        <div className="flex flex-col w-full h-auto">
-                            {foods ? <DataTable deleteFunction={(id: number) => {setCurrFoodID(id); setDeletePopup(true);}} editFunction={editFood} data={foods} headers={FoodItemHeaders}></DataTable> : ""}
-                        </div>
+            <AdminBase navbarTitle="Foods" navbarProps={<></>}>
+                <div className="flex items-center justify-start h-12 w-full rounded-t bg-white dark:bg-admin-dark-background dark:text-ad-golden px-3 pl-8">
+                    <span className="text-lg font-semibold dark:text-ad-golden">Food Items</span>
+                    <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:bg-ad-golden dark:text-black rounded-full px-1 py-1">
+                        <button onClick={()=>{setAddPopup(true)}}>Add Food Item<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
                     </div>
                 </div>
-            </div>
+                <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
+                    <div className="flex flex-col w-full h-auto">
+                        {foods ? <DataTable deleteFunction={(id: number) => {setCurrFoodID(id); setDeletePopup(true);}} editFunction={editFood} data={foods} headers={FoodItemHeaders}></DataTable> : ""}
+                    </div>
+                </div>
+            </AdminBase>
             {deletePopup ? 
                 <>
                     <PopupOverlay></PopupOverlay>
                     <DeletePopup trigger={deletePopup} setTrigger={setDeletePopup} submitMethod={deleteFuntion}>
                     </DeletePopup>
                 </> : ""}
-        </div>
+        </section>
     )
 }
