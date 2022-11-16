bfb1433317ad2d55b294775d1e93b3dc3ec4092d
moenassar0 <mnassar57@gmail.com>
2022-11-16 20:07:02 +0200

    frontend: cleaned up component

--- autodiet-frontend/src/pages/admin/AdminUsers.tsx
+++ autodiet-frontend/src/pages/admin/AdminUsers.tsx
@@ -14,6 +14,7 @@ import { adminNavbarLinks } from "../../types/consts";
 import { PopupOverlay } from "../../components/utility/PopupOverlay";
 import { Popup } from "../../components/utility/Popup";
 import { DeletePopup } from "../../components/utility/DeletePopup";
+import { AdminBase } from "../../layouts/AdminBase";
 
 export const AdminUsers = () => {
     const [users, setUsers] = useState([]);
@@ -46,33 +47,19 @@ export const AdminUsers = () => {
     }, [])
 
     return(
-        <div className="flex h-screen w-full">
+        <section>
             <AddUserPopup edit={false} trigger={addUsersPopup} setTrigger={setAddUsersPopup}/>
             <AddUserPopup userID={userID} edit={true} trigger={editUsersPopup} setTrigger={setEditUsersPopup}/>
-            <SideNavbar navbarlinks={adminNavbarLinks}/>
-            <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
-                <AdminTopNavbar title="" username="Admin"> 
-                </AdminTopNavbar>
-                <div className="h-5/6 grow w-full bg-admin-grey-background px-4 py-4 dark:bg-[#1F1F1F]">
-                    <div className="flex items-center justify-start h-12 w-full rounded-t bg-white px-3 pl-8 dark:bg-admin-dark-background dark:text-ad-golden">
-                        <span className="text-lg font-semibold">Users</span>
-                        <div className="flex w-28 justify-center ml-auto h-2/3 bg-admin-button text-white hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1">
-                            <button onClick={() => {setAddUsersPopup(true)}}>Add User<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>
-                        </div>
-                    </div>
-                    <div className="flex h-5/6 grow w-full overflow-y-scroll rounded drop-shadow">
-                        <div className="flex flex-col w-full h-auto">
-                            {users ? <DataTable editFunction={editfunc} deleteFunction={(id:number)=>{setUserID(id); setDeleteUsersPopup(true)}} deletePopup={deleteUsersPopup} setDeletePopup={setDeleteUsersPopup} data={users} headers={UserHeaders}></DataTable> : ""}
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {deleteUsersPopup ? 
-            <>
+            <AdminBase navbarProps={<></>} navbarTitle={"Users"} 
+            addButton={<button onClick={() => {setAddUsersPopup(true)}}>Add User<FontAwesomeIcon className="ml-2" icon={faPlus}/></button>}
+            dataTable={users ? <DataTable editFunction={editfunc} deleteFunction={(id:number)=>{setUserID(id); setDeleteUsersPopup(true)}} deletePopup={deleteUsersPopup} setDeletePopup={setDeleteUsersPopup} data={users} headers={UserHeaders}></DataTable> : ""}>
+                {deleteUsersPopup ? 
+                <>
                 <PopupOverlay></PopupOverlay>
                 <DeletePopup trigger={deleteUsersPopup} setTrigger={setDeleteUsersPopup} submitMethod={deletefunc}>
                 </DeletePopup>
-            </> : ""}
-        </div>
+                </> : ""}
+            </AdminBase>
+        </section>
     )
 }
