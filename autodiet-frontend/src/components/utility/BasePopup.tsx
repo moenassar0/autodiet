ce050107c3eb170543eb95ffd04737f523150eed
moenassar0 <mnassar57@gmail.com>
2022-11-17 23:55:17 +0200

    frontend: cleaned up styling

--- autodiet-frontend/src/components/utility/BasePopup.tsx
+++ autodiet-frontend/src/components/utility/BasePopup.tsx
@@ -1,19 +1,24 @@
 import { faClose } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Button } from "./Button"
 
 export const BasePopup: React.FC<{closeButton?:any, message: string, children: any, title: string, trigger: any, setTrigger: any, submitMethod: () => {}, submitButtonTitle: string}> = ({closeButton, submitButtonTitle, submitMethod, title, setTrigger, trigger, children, message}) => {
+    const styles ={
+        popupContainer: "absolute flex items-center justify-center w-full h-auto z-20 min-h-screen",
+        popupContent: "w-4/5 sm:w-3/6 max-h-screen flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg",
+        popupTitle: "flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black",
+        buttonDiv: "flex w-full h-16 justify-end items-center py-2 px-2"
+    }
     return(
-        <div className="absolute flex items-center justify-center w-full h-auto z-20 min-h-screen">
-            <div className="w-4/5 sm:w-3/6 max-h-screen flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
-                <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
+        <div className={styles.popupContainer}>
+            <div className={styles.popupContent}>
+                <div className={styles.popupTitle}>
                     <span>{title + "   "} {message ? message : ""}</span>
                     <span className="ml-auto">{closeButton ? closeButton : <FontAwesomeIcon onClick={() => {setTrigger(false)}} className="cursor-pointer text-slate-500 hover:text-slate-800"icon={faClose}></FontAwesomeIcon>}</span>
                 </div>
                 {children}
-                <div className="flex w-full h-16 justify-end items-center py-2 px-2">
-                    <button onClick={submitMethod} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">
-                        {submitButtonTitle}
-                    </button>
+                <div className={styles.buttonDiv}>
+                    <Button title={submitButtonTitle} onclickMethod={submitMethod} styling="" />
                 </div>
             </div>
         </div>
