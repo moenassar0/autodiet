dfbdfcbeaf21e415c5ccf6133637efe3faa3c00f
moenassar0 <mnassar57@gmail.com>
2022-11-17 17:09:32 +0200

    frontend: added placeholder prop

--- autodiet-frontend/src/components/utility/ProfileInput.tsx
+++ autodiet-frontend/src/components/utility/ProfileInput.tsx
@@ -1,9 +1,9 @@
-export const ProfileInput: React.FC<{title: string, hook: string, setHook: any}> = ({title, hook, setHook,})  => {
+export const ProfileInput: React.FC<{placeholder: string, title: string, hook: string, setHook: any}> = ({placeholder, title, hook, setHook,})  => {
     return(
         <div className="flex items-center w-full h-24">
             <div className="flex items-center w-1/5 h-full font-medium dark:text-ad-golden text-black">{title}</div>
             <div className="w-4/5 flex sm:w-3/5 h-1/2 justify-start drop-shadow-md">
-                <input type="text" value={hook} onChange={(e) => setHook(e.target.value)} className="w-1/2 grow h-full rounded px-1 py-1"></input>
+                <input placeholder={placeholder} type="text" value={hook} onChange={(e) => setHook(e.target.value)} className="w-1/2 grow h-full rounded px-1 py-1"></input>
             </div>
         </div>
     )
