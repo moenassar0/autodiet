4e219d43a7296b71c8b71e7ce63330110c6e6219
moenassar0 <mnassar57@gmail.com>
2022-11-17 19:12:11 +0200

    frontend: base multiform steps component added

---
+++ autodiet-frontend/src/components/utility/Steps.tsx
@@ -0,0 +1,14 @@
+export const Steps = () => {
+    const styles = {
+        inactive: "bg-slate-400",
+        active: "bg-admin-main",
+        step: "z-20 flex items-center justify-center rounded-full h-full w-10 text-white cursor-pointer "
+    }
+    return(
+        <div className="relative w-full h-auto">
+            <div className="relative flex w-full h-10 justify-between">
+
+            </div>
+        </div>
+    )
+} 
