0c6aed7f3059b1c15854c6350b12ff9f2b80a927
moenassar0 <mnassar57@gmail.com>
2022-11-17 23:58:55 +0200

    frontend: cleaned up styling classes

--- autodiet-frontend/src/components/utility/InputField.tsx
+++ autodiet-frontend/src/components/utility/InputField.tsx
@@ -3,17 +3,22 @@ import { InputFieldInterface } from "../../types/types"
 
 //change to interface
 const InputField: React.FC<InputFieldInterface> = ({title, error, state, setHook, valid, placeholder}) => {
-
+    const styles = {
+        container: "flex items-center place-content-between w-full h-10 px-2 py-1",
+        title: "w-2/5 text-admin-main dark:text-ad-golden text-lg",
+        inputFieldNormal: "h-full w-3/5 py-2 px-4 bg-admin-main text-white dark:bg-black rounded focus:outline-none focus:ring-2 focus:border-admin-button focus:ring-admin-button dark:focus:border-ad-golden dark:focus:ring-ad-golden ",
+        inputFieldInvalid: "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600"
+    }
     return(
         <>
-            <div className="flex items-center place-content-between w-full h-10 px-2 py-1">
-                <span className="w-2/5 text-admin-main dark:text-ad-golden text-lg">{title}</span>
+            <div className={styles.container}>
+                <span className={styles.title}>{title}</span>
                 <input
                     placeholder={placeholder}
                     value={state} 
                     onChange={(e) => setHook(e.target.value)} 
-                    className={"h-full w-3/5 py-2 px-4 bg-admin-main text-white dark:bg-black rounded focus:outline-none focus:ring-2 focus:border-admin-button focus:ring-admin-button dark:focus:border-ad-golden dark:focus:ring-ad-golden" +
-                    (!valid && state ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="text">
+                    className={styles.inputFieldNormal +
+                    (!valid && state ? styles.inputFieldInvalid : "")} type="text">
                 </input>
             </div>
             {!valid && state ? <p className="w-3/5 flex self-end text-red-600">{error}</p> : ""}
