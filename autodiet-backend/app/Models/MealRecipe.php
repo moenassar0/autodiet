6841634daf169f04ec42c8e1ab7d7a24c03b081f
moenassar0 <mnassar57@gmail.com>
2022-11-17 23:33:20 +0200

    backend: fillable property added

--- autodiet-backend/app/Models/MealRecipe.php
+++ autodiet-backend/app/Models/MealRecipe.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class MealRecipe extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'meal_id',
+        'recipe_item_id',
+        'multiplier',
+    ];
 }
