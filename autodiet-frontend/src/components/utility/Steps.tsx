795d19ff9fcd284b3387b16b2f26356024e81794
moenassar0 <mnassar57@gmail.com>
2022-11-17 19:12:30 +0200

    frontend: divs added

--- autodiet-frontend/src/components/utility/Steps.tsx
+++ autodiet-frontend/src/components/utility/Steps.tsx
@@ -7,7 +7,10 @@ export const Steps = () => {
     return(
         <div className="relative w-full h-auto">
             <div className="relative flex w-full h-10 justify-between">
-
+                <div className="px-2 absolute top-1/2 left-1/2 bg-slate-200 h-1 w-full h-auto -translate-x-1/2 -translate-y-1/2 z-10"></div>
+                <div className={styles.step + styles.active}>1</div>
+                <div className={styles.step + styles.inactive}>2</div>
+                <div className={styles.step + styles.inactive}>3</div>
             </div>
         </div>
     )
