760d984558bfa264ee3d1a929115a65db9a918d2
moenassar0 <mnassar57@gmail.com>
2022-11-17 16:57:39 +0200

    frontend: admin base component added

---
+++ autodiet-frontend/src/pages/admin/AdminRecipes.tsx
@@ -0,0 +1,9 @@
+import { AdminBase } from "../../layouts/AdminBase"
+
+export const AdminRecipes = () => {
+    return(
+        <AdminBase navbarProps={<></>} navbarTitle="Recipes" addButton={<></>} dataTable={<></>}>
+            
+        </AdminBase>
+    )
+}
