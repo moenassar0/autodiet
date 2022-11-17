0c5caf0045c8090665bc739b8216a5290c79a0e0
moenassar0 <mnassar57@gmail.com>
2022-11-17 22:43:33 +0200

    frontend: updated popup

--- autodiet-frontend/src/components/utility/Popup.tsx
+++ autodiet-frontend/src/components/utility/Popup.tsx
@@ -2,29 +2,21 @@ import { InputFieldInterface } from "../../types/types";
 import InputField from "./InputField";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClose } from '@fortawesome/free-solid-svg-icons'
+import { BasePopup } from "./BasePopup";
 
 export const Popup: React.FC<{children: any, edit: boolean, message: string, submitMethod: any, title: string, inputs: Array<InputFieldInterface>}> = ({children, edit, title, inputs, submitMethod, message}) => {
     return(
-        <div className="absolute flex items-center justify-center w-full h-auto z-20 min-h-screen">
-        <div className="w-4/5 sm:w-3/6 h-auto flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg">
-            <div className="flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black">
-                <span>{title + "   "} {message ? message : ""}</span>
-                <span className="ml-auto">{children}</span>
-            </div>
-            {
-                inputs.map((input, i) => (
-                    <InputField key={i} title={input.title} 
-                    error={input.error} setHook={input.setHook} 
-                    state={input.state} 
-                    valid={input.valid}></InputField>
-                ))
-            }
-            <div className="flex w-full h-16 justify-end items-center py-2 px-2">
-                <button onClick={submitMethod} className="flex w-24 h-8 items-center justify-center px-2 py-2 bg-admin-button text-white dark:text-black dark:bg-ad-golden rounded-2xl ml-0">
-                    {(edit ? "Edit" : "Add")}
-                </button>
-            </div>
-        </div>
-    </div>
+        <BasePopup closeButton={children} message={message} title={title} submitMethod={submitMethod}
+        submitButtonTitle={(edit ? "Edit" : "Add")} trigger={null} setTrigger={null}>
+        {
+            inputs.map((input, i) => (
+                <InputField key={i} title={input.title} 
+                error={input.error} setHook={input.setHook} 
+                state={input.state} 
+                valid={input.valid}></InputField>
+            ))
+        }
+        </BasePopup>
+
     )
 }
