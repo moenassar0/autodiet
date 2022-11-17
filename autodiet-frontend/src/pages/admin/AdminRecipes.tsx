d969c375132101894c3312b057d71d9f3cd34204
moenassar0 <mnassar57@gmail.com>
2022-11-17 17:09:20 +0200

    frontend: fetched meals

--- autodiet-frontend/src/pages/admin/AdminRecipes.tsx
+++ autodiet-frontend/src/pages/admin/AdminRecipes.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import { getMeals } from "../../api/services/Meals"
+import { AdminTopNavbar } from "../../components/admin/AdminTopNavbar"
+import { SideNavbar } from "../../components/admin/SideNavbar"
 import { AdminBase } from "../../layouts/AdminBase"
+import { adminNavbarLinks } from "../../types/consts"
 
 export const AdminRecipes = () => {
+    
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        fetchMeals();
+    }, [])
+
+    const fetchMeals = async () => {
+        setMeals((await getMeals())?.response.meals);
+    }
+
     return(
-        <AdminBase navbarProps={<></>} navbarTitle="Recipes" addButton={<></>} dataTable={<></>}>
-            
-        </AdminBase>
+        <div className="flex h-screen w-full">
+            <SideNavbar navbarlinks={adminNavbarLinks}/>
+            <div className="w-4/6 sm:w-5/6 flex flex-col grow h-screen">
+                <AdminTopNavbar title={"Recipes"} username="Admin">
+                </AdminTopNavbar>
+                <div className="h-5/6 grow w-full bg-admin-grey-background dark:bg-[#1F1F1F] px-4 py-4">
+                    {meals?.map((meal: any) => (<div>{meal.title}</div>))}
+                </div>
+            </div>
+        </div>
     )
 }
