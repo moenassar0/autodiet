b7ca59ad9bbb40caf463dc77d58174b0c4c41752
moenassar0 <mnassar57@gmail.com>
2022-11-17 21:57:17 +0200

    frontend: updated button component

--- autodiet-frontend/src/components/utility/Button.tsx
+++ autodiet-frontend/src/components/utility/Button.tsx
@@ -1,6 +1,6 @@
 export const Button: React.FC<{styling:string, title: string, onclickMethod: any}> = ({title, onclickMethod, styling = ""}) => {
     return(
-        <button onClick={onclickMethod} className={(styling) + ' ease-in duration-150 flex w-28 justify-center items-center h-10 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium'}>
+        <button onClick={onclickMethod} className={' ease-in duration-150 flex w-28 justify-center items-center h-10 text-white bg-admin-button hover:bg-admin-hoveredbutton dark:text-black dark:bg-ad-golden rounded-full px-1 py-1 font-medium ' + (styling)}>
             {title}
         </button>
     )
