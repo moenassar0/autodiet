1b835a9e547fda9f13fbc13ab0cd90a01601b89b
moenassar0 <mnassar57@gmail.com>
2022-11-18 20:32:03 +0200

    frontend: update styling

--- autodiet-frontend/src/components/utility/BasePopup.tsx
+++ autodiet-frontend/src/components/utility/BasePopup.tsx
@@ -2,16 +2,16 @@ import { faClose } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Button } from "./Button"
 
-export const BasePopup: React.FC<{closeButton?:any, message: string, children: any, title: string, trigger: any, setTrigger: any, submitMethod: any, submitButtonTitle: string}> = ({closeButton, submitButtonTitle, submitMethod, title, setTrigger, trigger, children, message}) => {
+export const BasePopup: React.FC<{buttonChildren?:any, closeButton?:any, message: string, children: any, title: string, trigger: any, setTrigger: any, submitMethod: any, submitButtonTitle: string}> = ({buttonChildren, closeButton, submitButtonTitle, submitMethod, title, setTrigger, trigger, children, message}) => {
     const styles ={
-        popupContainer: "absolute flex items-center justify-center w-full h-auto z-20 min-h-screen",
-        popupContent: "w-full max-h-screen flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg",
-        popupTitle: "flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black",
-        buttonDiv: "flex w-full h-16 justify-end items-center py-2 px-2"
+        popupContainer: "absolute flex items-center justify-center w-full h-auto z-20 min-h-screen rounded",
+        popupContent: "w-full h-screen flex flex-wrap bg-white content-start dark:bg-admin-dark-background text-admin-main dark:text-ad-golden overflow-auto gap-2 rounded-t-lg",
+        popupTitle: "flex w-full h-16 text-xl font-medium items-center px-2 rounded-t-lg border-b border-black dark:bg-admin-dark-background text-admin-main dark:text-ad-golden",
+        buttonDiv: "grow flex w-full h-16 justify-end items-center py-2 px-2"
     }
     return(
         <div className={styles.popupContainer}>
-            <div className="flex-col w-4/5 max-h-screen flex bg-white dark:bg-admin-background">
+            <div className="flex-col w-4/5 max-h-screen flex bg-white dark:bg-admin-dark-background rounded">
                 <div className={styles.popupTitle}>
                     <span>{title + "   "} {message ? message : ""}</span>
                     <span className="ml-auto">{closeButton ? closeButton : <FontAwesomeIcon onClick={() => {setTrigger(false)}} className="cursor-pointer text-slate-500 hover:text-slate-800"icon={faClose}></FontAwesomeIcon>}</span>
@@ -20,6 +20,7 @@ export const BasePopup: React.FC<{closeButton?:any, message: string, children: a
                     {children}
                 </div>
                 <div className={styles.buttonDiv}>
+                    {buttonChildren}
                     <Button title={submitButtonTitle} onclickMethod={submitMethod} styling="" />
                 </div>
             </div>
