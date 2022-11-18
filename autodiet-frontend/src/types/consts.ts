1fc433988b5267d5e59858a90f2ebd1786b21b92
moenassar0 <mnassar57@gmail.com>
2022-11-18 19:29:03 +0200

    frontend: updated navbarlinks

--- autodiet-frontend/src/types/consts.ts
+++ autodiet-frontend/src/types/consts.ts
@@ -1,4 +1,4 @@
-import { faBookOpen, faCalendarDays, faMessage, faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faNoteSticky, faBookOpen, faCalendarDays, faMessage, faRightFromBracket, faChartLine, faDrumstickBite, faPlateWheat, faUser } from '@fortawesome/free-solid-svg-icons'
 
 export const userNavbarLinks = [
     { path: "/user/home", icon: faCalendarDays, title: "Mealplan" },
@@ -6,6 +6,7 @@ export const userNavbarLinks = [
     { path: "/user/meals", icon: faPlateWheat, title: "Meals" },
     { path: "/user/foods", icon: faDrumstickBite, title: "Foods" },
     { path: "/user/weight", icon: faChartLine, title: "Weight" },
+    { path: "/user/dietlog", icon: faNoteSticky, title: "Diet Log" },
     { path: "/user/chat", icon: faMessage, title: "Chat" },
     { path: "/user/logout", icon: faRightFromBracket, title: "Logout" },
 ];
