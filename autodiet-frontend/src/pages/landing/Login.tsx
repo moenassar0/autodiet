34eb4c2e2245ddb0b9e5f9da08f1091bba3dd649
moenassar0 <mnassar57@gmail.com>
2022-11-21 21:18:45 +0200

    frontend: code cleanup and use of components

--- autodiet-frontend/src/pages/landing/Login.tsx
+++ autodiet-frontend/src/pages/landing/Login.tsx
@@ -4,6 +4,8 @@ import { Navbar } from "../../components/landing/Navbar"
 import { useNavigate, useRoutes } from 'react-router-dom';
 import { useUser } from "../../context/UserContext";
 import { LandingBase } from "../../layouts/LandingBase";
+import { loginUser } from "../../api/services/Users";
+import InputField from "../../components/utility/InputField";
 
 export const Login = () => {
 
@@ -15,22 +17,21 @@ export const Login = () => {
     
     const navigate = useNavigate();
 
+    const goToRegisterPage = () => {
+        navigate("/register");
+    }
+
     const login = async () => {
-        try{
-            setWaitingForResponse(true);
-            const response = await axios.post('/login', {email, password});
-            console.log(response.data);
-            const user2 = {email: response.data.user.email, username: response.data.user.username}
-            setUser(user2);
-            console.log("user", user);
-            localStorage.setItem("token", response.data.token.original.access_token)
-            if(response.data.user.user_role === "User")
-            navigate("/user/home");
-            if(response.data.user.user_role === "Admin")
-            navigate("/admin/users");
-        }catch(err){
-            console.log("Error from http request: ", err);
-        }
+        const response = await loginUser({email, password});
+        let user = response?.response.user;
+        user = {email: user.email, username: user.username, user_role: user.user_role}
+        setUser(user);
+        console.log("user", user);
+        localStorage.setItem("token", response?.response.token.original.access_token)
+        if(user.user_role === "User")
+        navigate("/user/home");
+        if(user.user_role === "Admin")
+        navigate("/admin/users");
         setWaitingForResponse(false);
     }
 
@@ -50,10 +51,9 @@ export const Login = () => {
                             <input placeholder="Password" type="password" onChange={(e) => setPassword(e.target.value)} className="w-4/5 py-2 px-4 bg-black text-white rounded-t"></input>
                         </div>
                         <div className="flex w-full justify-center">
-                        <button onClick={login} className="flex justify-center w-4/5 py-2 px-4 bg-ad-golden text-black rounded">{waitingForResponse ? <img className="h-6 w-6" src="../loading1.gif"></img> : "Login"}</button>
+                            <button onClick={login} className="flex justify-center w-4/5 py-2 px-4 bg-ad-golden text-black rounded">{waitingForResponse ? <img className="h-6 w-6" src="../loading1.gif"></img> : "Login"}</button>
                         </div>
-                        
-                        <p className="w-4/5 text-ad-golden">Create a new account?</p>
+                        <p onClick={goToRegisterPage} className="w-4/5 text-ad-golden hover:cursor-pointer hover:underline">Create a new account?</p>
                     </div>
                 </div>
         </LandingBase>
--- autodiet-frontend/src/pages/landing/Register.tsx
+++ autodiet-frontend/src/pages/landing/Register.tsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react";
-import axios from "../../api/axios"
-import { Navbar } from "../../components/landing/Navbar"
 import {checkEmail, checkPassword, checkStringLength} from "../../HelperFunctions";
 import InputField from "../../components/utility/InputField"
 import { useTheme } from "../../context/ThemeContext";
-import { useNavigate } from "react-router-dom";
+import { registerUser } from "../../api/services/Users";
+import { LandingBase } from "../../layouts/LandingBase";
 
 export const Register = () => {
     const {themeType, setCurrentTheme} = useTheme();
-    const navigate = useNavigate();
     
     const [username, setUsername] = useState('');
     const [validUsername, setValidUsername] = useState(false);
@@ -19,6 +17,8 @@ export const Register = () => {
     const [password, setPassword] = useState('');
     const [validPassword, setValidPassword] = useState(false);
     const [rPassword, setRPassword] = useState('');
+
+    const [message, setMessage] = useState('');
 
     const [waitingForResponse, setWaitingForResponse]  = useState(false);
 
@@ -31,73 +31,33 @@ export const Register = () => {
     }, [username, email, password])
 
     const register = async () => {
-        try{
-            setWaitingForResponse(true);
-            if(validEmail && validPassword && validUsername && password === rPassword){
-                const response = await axios.post('/users', {email, password, user_role: "User", username});
-                console.log(response);
-            }
-        }catch(err){
-            console.log("Error from http request: ", err);
+        setWaitingForResponse(true);
+        if(validEmail && validPassword && validUsername && password === rPassword){
+            const data = {email, password, user_role: "User", username};
+            const response = await registerUser(data);
+            if(response?.response) setMessage("Successfully registered!")
+            else setMessage("Registeration error, please try again!")
         }
         setWaitingForResponse(false);
     }
 
     return(
-        <div className="flex flex-col w-full h-screen">
-            <div className="flex items-center bg-black h-14 w-full p-2 z-20">
-                <div className="w-26 h-12"><img className="w-full h-full" src="../AutoDiet-1.png"></img></div>
-                <div className="flex h-full w-1/2 grow justify-end">
-                    <button onClick={() => {navigate("/login")}} className="w-24 h-full bg-ad-golden font-medium text-black rounded-full hover:opacity-80">LOGIN</button>
+        <LandingBase>
+            <div className="z-10 flex flex-col w-full max-w-lg min-h-96 h-auto items-center bg-ad-lightgrey rounded-t">
+                <div className="flex items-center justify-center w-full h-12 bg-black mb-8 rounded-t text-ad-golden font-bold text-lg">
+                    Create a new account
+                </div>
+                <div className="flex flex-col w-full justify-center gap-10 px-2">
+                    <InputField title="Email" setHook={setEmail} state={email} valid={validEmail} error="Invalid Email!" type="text"></InputField>
+                    <InputField title="Username" valid={validUsername} type="text" setHook={setUsername} state={username} error={"Usernames should be atleast 8 characters long!"}></InputField>
+                    <InputField type="password" state={password} title="Password" valid={validPassword} setHook={setPassword} error="Passwords should include one uppercase character and be atleast 8 characters long!"/>
+                    <InputField type="password" state={rPassword} setHook={setRPassword} valid={(password === rPassword)} error="Passwords don't match!" title="Repeat Password"></InputField>
+                    <div className="flex items-center self-end w-3/5 h-10 px-1">
+                        <button onClick={register} className="w-full h-full mb-10 bg-ad-golden text-black rounded">{waitingForResponse ? <img className="h-6 w-6" src="../loading1.gif"></img> : "Register"}</button>
+                    </div>
+                    {message ? message :    ""}
                 </div>
             </div>
-            <div className="flex items-center justify-center w-full h-5/6 grow relative">
-                <div className="grad"></div>
-                <div className="z-10 flex flex-col w-full max-w-lg min-h-96 h-auto items-center bg-ad-lightgrey rounded-t">
-                    <div className="flex items-center justify-center w-full h-12 bg-black mb-8 rounded-t text-ad-golden font-bold text-lg">
-                        Create a new account
-                    </div>
-                    <div className="flex flex-col w-full justify-center gap-10 px-2">
-                        <div className="flex items-center place-content-between w-full h-10 px-2">
-                            <span className="w-2/5 text-ad-golden text-lg">Email</span>
-                            <input 
-                                
-                                onChange={(e) => setEmail(e.target.value)} 
-                                className={"w-3/5 py-2 px-4 text-white bg-black rounded focus:outline-none focus:ring-2 focus:border-ad-golden focus:ring-ad-golden" +
-                                (!validEmail && email ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="email">
-                            </input>
-                        </div>
-                        {!validEmail && email ? <div className="w-3/5 flex self-end text-red-600 font-bold">Invalid Email!</div> : ""}
-                        <div className="flex items-center place-content-between w-full h-10 px-2">
-                            <span className="w-2/5 text-ad-golden text-lg">Username</span>
-                            <input 
-                            
-                                onChange={(e) => setUsername(e.target.value)} 
-                                className={"w-3/5 py-2 px-4 text-white bg-black rounded focus:outline-none focus:ring-2 focus:border-ad-golden focus:ring-ad-golden" +
-                                (!validUsername && username ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="text">
-                            </input>
-                        </div>
-                        {!validUsername && username ? <div className="w-3/5 flex self-end text-red-600 font-bold">Usernames should be atleast 8 characters long!</div> : ""}
-                        {<InputField type="password" state={password} title="Password" valid={validPassword} setHook={setPassword} error="Passwords should include one uppercase character and be atleast 8 characters long!"/>}
-                        <div className="flex items-center place-content-between w-full h-10 px-2">
-                            <span className="w-2/5 text-ad-golden text-lg">Repeat password</span>
-                            <input
-                                onChange={(e) => setRPassword(e.target.value)} 
-                                className={"w-3/5 py-2 px-4 text-white bg-black rounded focus:outline-none focus:ring-2 focus:border-ad-golden focus:ring-ad-golden" +
-                                (!(password === rPassword) && rPassword ? "focus:border-red-500 focus:ring-red-500 border-red-500 text-red-600" : "")} type="password">
-                            </input>
-                        </div>
-                        {!(password === rPassword) && rPassword ? <p className="w-3/5 flex self-end text-red-600">Passwords don't match!</p> : ""}
-                        <div className="flex items-center self-end w-3/5 h-10 px-1">
-                            <button onClick={register} className="w-full h-full mb-10 bg-ad-golden text-black rounded">{waitingForResponse ? <img className="h-6 w-6" src="../loading1.gif"></img> : "Register"}</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        </LandingBase>
     )
 }
-/*
-
-
-*/
